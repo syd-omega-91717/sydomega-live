5bdaf6baf3a77f35b2ae8a2d9ee36e9520f0c162
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-26 23:11:46 +0300

    Create express.d.ts

---
+++ backend/src/types/express.d.ts
@@ -0,0 +1,19 @@
+import { User } from "@supabase/supabase-js";
+
+declare global {
+
+    namespace Express {
+
+        interface Request {
+
+            user?: User;
+
+            requestId?: string;
+
+        }
+
+    }
+
+}
+
+export {};

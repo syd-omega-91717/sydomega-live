d0ee5d603edf33d803d1f0af8c2e40abc3e58b5a
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-27 04:38:15 +0300

    Update approval.controller.ts

--- backend/src/controllers/approval.controller.ts
+++ backend/src/controllers/approval.controller.ts
@@ -1,1 +1,73 @@
+import { Request, Response } from "express";
 
+import * as Approval from "../services/approval.service";
+
+export async function pending(
+
+    req: Request,
+
+    res: Response
+
+) {
+
+    const data = await Approval.pending();
+
+    res.json({
+
+        success: true,
+
+        data
+
+    });
+
+}
+
+export async function approve(
+
+    req: any,
+
+    res: Response
+
+) {
+
+    await Approval.approve(
+
+        req.params.id,
+
+        req.user.id
+
+    );
+
+    res.json({
+
+        success: true
+
+    });
+
+}
+
+export async function reject(
+
+    req: any,
+
+    res: Response
+
+) {
+
+    await Approval.reject(
+
+        req.params.id,
+
+        req.user.id,
+
+        req.body.reason
+
+    );
+
+    res.json({
+
+        success: true
+
+    });
+
+}

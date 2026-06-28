9887c5ac18f3cd8c64dd5ffb1360e763c1d07fc5
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-29 00:17:44 +0300

    Create index.ts

---
+++ backend/src/modules/identity/application/services/index.ts
@@ -0,0 +1,6 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/application/services/index.ts
+// NEW FILE
+// ============================================================================
+
+export * from "./identity.service.js";

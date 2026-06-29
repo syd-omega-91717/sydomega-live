50d806145a87e405140bc2fc942b092ccf9d1731
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-29 16:32:56 +0300

    Create organization.repository.spec.ts

---
+++ backend/tests/integration/organization.repository.spec.ts
@@ -0,0 +1,28 @@
+// ============================================================================
+// FILE: /backend/tests/integration/organization.repository.spec.ts
+// NEW FILE
+// ============================================================================
+
+import { describe,it }
+
+from "vitest";
+
+describe(
+
+    "Supabase Repository",
+
+    ()=>{
+
+        it(
+
+            "persists aggregate",
+
+            async()=>{
+
+            }
+
+        );
+
+    }
+
+);

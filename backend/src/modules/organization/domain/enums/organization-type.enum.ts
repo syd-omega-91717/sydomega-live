07e27d107798a679fbf3f4083bc6589c3be4bb72
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-29 08:24:26 +0300

    Create organization-type.enum.ts

---
+++ backend/src/modules/organization/domain/enums/organization-type.enum.ts
@@ -0,0 +1,19 @@
+// domain/enums/organization-type.enum.ts
+
+export enum OrganizationType {
+
+    PERSONAL = "personal",
+
+    COMPANY = "company",
+
+    ENTERPRISE = "enterprise",
+
+    UNIVERSITY = "university",
+
+    ACADEMY = "academy",
+
+    GOVERNMENT = "government",
+
+    NON_PROFIT = "non_profit"
+
+}

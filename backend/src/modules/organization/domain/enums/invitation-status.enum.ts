3fb657c377e49a3e3b5d67ee9dd36ec8cd3af5e2
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-29 08:25:43 +0300

    Create invitation-status.enum.ts

---
+++ backend/src/modules/organization/domain/enums/invitation-status.enum.ts
@@ -0,0 +1,15 @@
+// domain/enums/invitation-status.enum.ts
+
+export enum InvitationStatus {
+
+    PENDING = "pending",
+
+    ACCEPTED = "accepted",
+
+    DECLINED = "declined",
+
+    EXPIRED = "expired",
+
+    CANCELLED = "cancelled"
+
+}

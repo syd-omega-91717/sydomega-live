723a75e9be5557a0dddc64eca23911e9ba0a0e22
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 02:04:42 +0300

    Create userinfo.controller.ts

---
+++ backend/src/modules/oidc/presentation/controllers/userinfo.controller.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/oidc/presentation/controllers/userinfo.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class UserInfoController {
+
+    async userInfo(){}
+
+}

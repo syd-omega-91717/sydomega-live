bb0c0e75f4a892549b0a2faff9b1583a1b0607be
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-29 22:16:55 +0300

    Create user-id.ts

---
+++ backend/src/modules/identity/domain/value-objects/user-id.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/domain/value-objects/user-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+
+from "@/kernel/domain/unique-entity-id";
+
+export class UserId
+
+extends UniqueEntityId{
+
+}

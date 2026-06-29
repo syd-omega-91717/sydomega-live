4381104987a2582467bcb76af2835ad6a435fbb3
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-29 08:25:06 +0300

    Create membership-role.enum.ts

---
+++ backend/src/modules/organization/domain/enums/membership-role.enum.ts
@@ -0,0 +1,21 @@
+// domain/enums/membership-role.enum.ts
+
+export enum MembershipRole {
+
+    FOUNDER = "founder",
+
+    OWNER = "owner",
+
+    ADMIN = "admin",
+
+    MANAGER = "manager",
+
+    EDITOR = "editor",
+
+    MEMBER = "member",
+
+    VIEWER = "viewer",
+
+    GUEST = "guest"
+
+}

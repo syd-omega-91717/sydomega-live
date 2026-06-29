0ab28e6b360aa72f794f0c8195255ce7e301b52f
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-29 22:14:53 +0300

    Create user-status.ts

---
+++ backend/src/modules/identity/domain/enums/user-status.ts
@@ -0,0 +1,20 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/domain/enums/user-status.ts
+// NEW FILE
+// ============================================================================
+
+export enum UserStatus {
+
+    Pending = "PENDING",
+
+    Active = "ACTIVE",
+
+    Suspended = "SUSPENDED",
+
+    Locked = "LOCKED",
+
+    Disabled = "DISABLED",
+
+    Deleted = "DELETED"
+
+}

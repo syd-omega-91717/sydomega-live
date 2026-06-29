61bd85d900add530e0903adcef8c034c9227798d
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-29 07:54:05 +0300

    Create command.ts

---
+++ backend/src/platform/pipeline/command.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/platform/pipeline/command.ts
+// NEW FILE
+// ============================================================================
+
+export interface Command {
+
+    readonly name: string;
+
+}

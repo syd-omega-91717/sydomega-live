38588575d48ce3e05b8f57b873a224013f2bd15d
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 02:05:11 +0300

    Create logout.controller.ts

---
+++ backend/src/modules/oidc/presentation/controllers/logout.controller.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/oidc/presentation/controllers/logout.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class LogoutController {
+
+    async endSession(){}
+
+}

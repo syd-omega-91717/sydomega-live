37cb21e98f99b4d4c79c011bffac552c7fa14ad9
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 02:05:47 +0300

    Create discovery.controller.ts

---
+++ backend/src/modules/oidc/presentation/controllers/discovery.controller.ts
@@ -0,0 +1,12 @@
+// ============================================================================
+// FILE: /backend/src/modules/oidc/presentation/controllers/discovery.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class DiscoveryController {
+
+    async configuration(){}
+
+    async jwks(){}
+
+}

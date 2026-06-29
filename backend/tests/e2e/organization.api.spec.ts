ac4f8d35ecbf77e5c546a11bb8359b0deffa4308
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-29 16:33:32 +0300

    Create organization.api.spec.ts

---
+++ backend/tests/e2e/organization.api.spec.ts
@@ -0,0 +1,28 @@
+// ============================================================================
+// FILE: /backend/tests/e2e/organization.api.spec.ts
+// NEW FILE
+// ============================================================================
+
+import { describe,it }
+
+from "vitest";
+
+describe(
+
+    "Organization REST API",
+
+    ()=>{
+
+        it(
+
+            "creates organization",
+
+            async()=>{
+
+            }
+
+        );
+
+    }
+
+);

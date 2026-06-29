bc1079cbfcdbcf8dce595eb1d8e5483d70ff9b79
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-29 15:40:31 +0300

    Create get-organization-statistics.query.ts

---
+++ backend/src/modules/organization/application/queries/get-organization-statistics.query.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/organization/application/queries/get-organization-statistics.query.ts
+// NEW FILE
+// ============================================================================
+
+export interface GetOrganizationStatisticsQuery {
+
+    organizationId: string;
+
+}

28fab0ea2f2cf249883d84c5221fef1491b55f85
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-29 22:31:00 +0300

    Create passkey.aggregate.ts

---
+++ backend/src/modules/identity/domain/aggregates/passkey.aggregate.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /backend/src/modules/identity/domain/aggregates/passkey.aggregate.ts
+// NEW FILE
+// ============================================================================
+
+export class PasskeyAggregate
+extends AggregateRoot<UserId>{
+
+    registerCredential(){}
+
+    authenticate(){}
+
+    removeCredential(){}
+
+    renameCredential(){}
+
+}

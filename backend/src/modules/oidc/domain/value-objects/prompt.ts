d11d303532d76050da77835df5eeff99e4d24a5b
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 02:07:45 +0300

    Create prompt.ts

---
+++ backend/src/modules/oidc/domain/value-objects/prompt.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/oidc/domain/value-objects/prompt.ts
+// NEW FILE
+// ============================================================================
+
+export enum Prompt {
+
+    None = "none",
+
+    Login = "login",
+
+    Consent = "consent",
+
+    SelectAccount = "select_account"
+
+}

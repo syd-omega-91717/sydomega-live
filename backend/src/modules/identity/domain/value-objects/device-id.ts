b7a1c124937aa118166848501fe43c81adef8354
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-29 22:24:09 +0300

    Create device-id.ts

---
+++ backend/src/modules/identity/domain/value-objects/device-id.ts
@@ -0,0 +1,13 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/domain/value-objects/device-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+
+from "@/kernel/domain/unique-entity-id";
+
+export class DeviceId
+extends UniqueEntityId{
+
+}

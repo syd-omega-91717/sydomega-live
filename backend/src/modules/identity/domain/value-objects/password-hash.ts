cb20f76f4f9fbd85b4117b2fb4a3a0c7c3e1a5a2
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-29 22:16:23 +0300

    Create password-hash.ts

---
+++ backend/src/modules/identity/domain/value-objects/password-hash.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/domain/value-objects/password-hash.ts
+// NEW FILE
+// ============================================================================
+
+export class PasswordHash{
+
+    constructor(
+
+        readonly value:string
+
+    ){}
+
+}

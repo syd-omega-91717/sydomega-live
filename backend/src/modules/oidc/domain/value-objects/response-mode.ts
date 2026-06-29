103cb4f5f4c4bd042960400947abf32c1472dc9e
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 02:08:46 +0300

    Create response-mode.ts

---
+++ backend/src/modules/oidc/domain/value-objects/response-mode.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/oidc/domain/value-objects/response-mode.ts
+// NEW FILE
+// ============================================================================
+
+export enum ResponseMode {
+
+    Query = "query",
+
+    Fragment = "fragment",
+
+    FormPost = "form_post"
+
+}

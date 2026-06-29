13c72b2bd025b7901d8981ac614ad9b1052559cf
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 02:08:16 +0300

    Create response-type.ts

---
+++ backend/src/modules/oidc/domain/value-objects/response-type.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/oidc/domain/value-objects/response-type.ts
+// NEW FILE
+// ============================================================================
+
+export enum ResponseType {
+
+    Code = "code"
+
+}

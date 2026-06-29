9928ffe806f1cf48129b8d0dc4430ae6aad8580a
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 02:04:13 +0300

    Create token.controller.ts

---
+++ backend/src/modules/oidc/presentation/controllers/token.controller.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/oidc/presentation/controllers/token.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class TokenController {
+
+    async token(){}
+
+}

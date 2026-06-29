7e81581bf231724aee91873ff04a8ed9063847de
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-29 15:38:41 +0300

    Create get-organization.query.ts

---
+++ backend/src/modules/organization/application/queries/get-organization.query.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/organization/application/queries/get-organization.query.ts
+// NEW FILE
+// ============================================================================
+
+export interface GetOrganizationQuery {
+
+    organizationId: string;
+
+}

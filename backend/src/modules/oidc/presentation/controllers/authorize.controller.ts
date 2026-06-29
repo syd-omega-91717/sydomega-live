542f68f9d5041ced493b5c8a95ece9260472f246
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 02:03:37 +0300

    Create authorize.controller.ts

---
+++ backend/src/modules/oidc/presentation/controllers/authorize.controller.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/oidc/presentation/controllers/authorize.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class AuthorizeController {
+
+    async authorize(){}
+
+}

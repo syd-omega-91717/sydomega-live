721b321bc10d0ee90eee7d6a2bfb5f2b671435c3
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-29 16:16:34 +0300

    Create widget.permissions.ts

---
+++ backend/src/modules/organization/widgets/widget.permissions.ts
@@ -0,0 +1,32 @@
+// ============================================================================
+// FILE: /backend/src/modules/organization/widgets/widget.permissions.ts
+// NEW FILE
+// ============================================================================
+
+export const OrganizationWidgetPermissions={
+
+    "organization-overview":[
+
+        "organization.read"
+
+    ],
+
+    "organization-members":[
+
+        "organization.member.read"
+
+    ],
+
+    "organization-storage":[
+
+        "organization.storage.read"
+
+    ],
+
+    "organization-subscription":[
+
+        "organization.subscription.read"
+
+    ]
+
+};

ecd2a547526dbc98a828cf08a97f697f5ccecf29
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 12:07:12 +0300

    Create api-key.routes.ts

---
+++ backend/src/modules/identity/presentation/routes/api-key.routes.ts
@@ -0,0 +1,12 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/presentation/routes/api-key.routes.ts
+// NEW FILE
+// ============================================================================
+
+GET     /apikeys
+
+POST    /apikeys
+
+POST    /apikeys/{id}/rotate
+
+DELETE  /apikeys/{id}

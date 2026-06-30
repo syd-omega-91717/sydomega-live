b06d0e718b380910260449bd950907f27b694a19
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 11:59:23 +0300

    Create rotate-api-key.command.ts

---
+++ backend/src/modules/identity/application/commands/rotate-api-key.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/application/commands/rotate-api-key.command.ts
+// NEW FILE
+// ============================================================================
+
+export class RotateApiKeyCommand {
+
+    constructor(
+
+        readonly apiKeyId:string
+
+    ){}
+
+}

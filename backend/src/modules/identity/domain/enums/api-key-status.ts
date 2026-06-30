c5de79dad125ae32022500b96063b858d76b5e34
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 11:54:59 +0300

    Create api-key-status.ts

---
+++ backend/src/modules/identity/domain/enums/api-key-status.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/domain/enums/api-key-status.ts
+// NEW FILE
+// ============================================================================
+
+export enum ApiKeyStatus {
+
+    Active="ACTIVE",
+
+    Revoked="REVOKED",
+
+    Expired="EXPIRED"
+
+}

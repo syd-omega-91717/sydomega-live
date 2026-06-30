970f4e5ba95476fa4b9fb6174781e0c4ebe2c161
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 01:29:47 +0300

    Create audit.controller.ts

---
+++ backend/src/modules/audit/presentation/controllers/audit.controller.ts
@@ -0,0 +1,12 @@
+// ============================================================================
+// FILE: /backend/src/modules/audit/presentation/controllers/audit.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class AuditController{
+
+    async search(){}
+
+    async export(){}
+
+}

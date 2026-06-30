1112e88618c4dce23e246af712defaa1d987bf63
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 22:49:18 +0300

    Create identity-analytics.controller.ts

---
+++ backend/src/modules/identity/presentation/controllers/identity-analytics.controller.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/presentation/controllers/identity-analytics.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class IdentityAnalyticsController{
+
+    async dashboard(){}
+
+    async metrics(){}
+
+    async export(){}
+
+}

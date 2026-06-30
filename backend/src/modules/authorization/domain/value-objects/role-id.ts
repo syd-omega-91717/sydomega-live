ab070052adf809f0d560ce1399102eddc063f595
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 02:33:15 +0300

    Create role-id.ts

---
+++ backend/src/modules/authorization/domain/value-objects/role-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/value-objects/role-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class RoleId
+extends UniqueEntityId {}

d3f92a81fc98ca4f62a57810cfad07e0c1712db4
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 08:36:15 +0300

    Create sso.controller.ts

---
+++ backend/src/modules/saml/presentation/controllers/sso.controller.ts
@@ -0,0 +1,12 @@
+// ============================================================================
+// FILE: /backend/src/modules/saml/presentation/controllers/sso.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class SsoController {
+
+    async login(){}
+
+    async consumeAssertion(){}
+
+}

f2f728f688a601e0515a509e284b5d7d4990f5d9
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 01:14:43 +0300

    Create identity-timeline-generated.event.ts

---
+++ backend/src/modules/identity/domain/events/identity-timeline-generated.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/domain/events/identity-timeline-generated.event.ts
+// NEW FILE
+// ============================================================================
+
+export class IdentityTimelineGeneratedEvent{
+
+    constructor(
+
+        readonly userId:string
+
+    ){}
+
+}

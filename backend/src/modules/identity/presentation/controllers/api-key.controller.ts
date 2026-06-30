1e0b29469462c1f8211afd92b4188d8480117c37
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 12:06:41 +0300

    Create api-key.controller.ts

---
+++ backend/src/modules/identity/presentation/controllers/api-key.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/presentation/controllers/api-key.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class ApiKeyController{
+
+    async create(){}
+
+    async list(){}
+
+    async rotate(){}
+
+    async revoke(){}
+
+}

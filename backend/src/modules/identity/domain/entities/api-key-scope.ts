de5b91551d5b0a68009176939f1643f8c2586edf
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 11:55:36 +0300

    Create api-key-scope.ts

---
+++ backend/src/modules/identity/domain/entities/api-key-scope.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/domain/entities/api-key-scope.ts
+// NEW FILE
+// ============================================================================
+
+export class ApiKeyScope {
+
+    constructor(
+
+        readonly value:string
+
+    ){}
+
+}

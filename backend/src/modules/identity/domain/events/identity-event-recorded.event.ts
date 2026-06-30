14a4e2e567af307a656023db751aa8a42c7f16d0
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 01:14:16 +0300

    Create identity-event-recorded.event.ts

---
+++ backend/src/modules/identity/domain/events/identity-event-recorded.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/domain/events/identity-event-recorded.event.ts
+// NEW FILE
+// ============================================================================
+
+export class IdentityEventRecordedEvent{
+
+    constructor(
+
+        readonly eventId:string
+
+    ){}
+
+}

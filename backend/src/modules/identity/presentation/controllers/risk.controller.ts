f9c8cf6c8d924f1fd29a25b2524a44e5ca309215
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 16:13:59 +0300

    Create risk.controller.ts

---
+++ backend/src/modules/identity/presentation/controllers/risk.controller.ts
@@ -0,0 +1,12 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/presentation/controllers/risk.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class RiskController{
+
+    async evaluate(){}
+
+    async history(){}
+
+}

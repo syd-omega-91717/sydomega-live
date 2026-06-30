5dac4d3dc1f528ed76378eb44bbed6aa37689e30
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 00:19:52 +0300

    Create session.gateway.ts

---
+++ backend/src/modules/identity/presentation/websocket/session.gateway.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/presentation/websocket/session.gateway.ts
+// NEW FILE
+// ============================================================================
+
+export class SessionGateway{
+
+    async connect(){}
+
+    async disconnect(){}
+
+    async broadcast(){}
+
+}

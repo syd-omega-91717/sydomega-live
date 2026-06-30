c0097e0778f2865146992d0b65c78191882b2b52
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 01:30:20 +0300

    Create audit.routes.ts

---
+++ backend/src/modules/audit/presentation/routes/audit.routes.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/audit/presentation/routes/audit.routes.ts
+// NEW FILE
+// ============================================================================
+
+GET     /audit/logs
+
+GET     /audit/export
+
+GET     /audit/{id}

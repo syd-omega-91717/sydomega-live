4b720ef8e4df37a8746f60caf791bbf485fdabd6
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 09:17:24 +0300

    Create scim-user-created.event.ts

---
+++ backend/src/modules/scim/domain/events/scim-user-created.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/scim/domain/events/scim-user-created.event.ts
+// NEW FILE
+// ============================================================================
+
+export class ScimUserCreatedEvent {
+
+    constructor(
+
+        readonly userId: string
+
+    ) {}
+
+}

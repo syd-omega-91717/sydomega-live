8af86b127bcbb84bca342ef1f8d18db5b98ae9b1
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 11:58:55 +0300

    Create revoke-api-key.command.ts

---
+++ backend/src/modules/identity/application/commands/revoke-api-key.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/application/commands/revoke-api-key.command.ts
+// NEW FILE
+// ============================================================================
+
+export class RevokeApiKeyCommand {
+
+    constructor(
+
+        readonly apiKeyId:string
+
+    ){}
+
+}

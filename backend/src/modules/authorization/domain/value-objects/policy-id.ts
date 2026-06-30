5b8d5973e63c46a6c0e3733e2c3d310a5579f458
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 02:19:45 +0300

    Create policy-id.ts

---
+++ backend/src/modules/authorization/domain/value-objects/policy-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/value-objects/policy-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class PolicyId
+extends UniqueEntityId {}

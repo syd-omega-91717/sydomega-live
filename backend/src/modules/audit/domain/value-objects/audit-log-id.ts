5ddfcc36989bbee99f010a8c8f5b1fb7081d24c0
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 01:22:41 +0300

    Create audit-log-id.ts

---
+++ backend/src/modules/audit/domain/value-objects/audit-log-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/audit/domain/value-objects/audit-log-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class AuditLogId
+extends UniqueEntityId {}

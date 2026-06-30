beeb015d9fff2255fc7561f6a88f04f49bf743f3
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 02:33:53 +0300

    Create permission-id.ts

---
+++ backend/src/modules/authorization/domain/value-objects/permission-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/value-objects/permission-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class PermissionId
+extends UniqueEntityId {}

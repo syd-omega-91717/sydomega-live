980a98764dd7990df64c5f5757cab3ab87cded3a
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 22:44:41 +0300

    Create identity-metrics-collected.event.ts

---
+++ backend/src/modules/identity/domain/events/identity-metrics-collected.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/domain/events/identity-metrics-collected.event.ts
+// NEW FILE
+// ============================================================================
+
+export class IdentityMetricsCollectedEvent{
+
+    constructor(
+
+        readonly timestamp:Date
+
+    ){}
+
+}

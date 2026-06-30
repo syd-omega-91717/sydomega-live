274a73d738a32a165768fec33d7daf8cfd9776c2
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 02:22:48 +0300

    Create policy-created.event.ts

---
+++ backend/src/modules/authorization/domain/events/policy-created.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/policy-created.event.ts
+// NEW FILE
+// ============================================================================
+
+export class PolicyCreatedEvent{
+
+    constructor(
+
+        readonly policyId:string
+
+    ){}
+
+}

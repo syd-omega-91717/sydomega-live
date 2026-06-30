21d2c313f8d6b4af16afb844e84bbbe858621413
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 09:17:57 +0300

    Create scim-user-deactivated.event.ts

---
+++ backend/src/modules/scim/domain/events/scim-user-deactivated.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/scim/domain/events/scim-user-deactivated.event.ts
+// NEW FILE
+// ============================================================================
+
+export class ScimUserDeactivatedEvent {
+
+    constructor(
+
+        readonly userId: string
+
+    ) {}
+
+}

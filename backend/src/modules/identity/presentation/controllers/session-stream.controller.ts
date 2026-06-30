1af795670d5efef05c980a29b53df3c4a6d6c287
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 00:20:24 +0300

    Create session-stream.controller.ts

---
+++ backend/src/modules/identity/presentation/controllers/session-stream.controller.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/presentation/controllers/session-stream.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class SessionStreamController{
+
+    async sessions(){}
+
+    async terminate(){}
+
+    async terminateAll(){}
+
+}

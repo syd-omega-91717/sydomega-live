c334eb24b1b2b68717ba1712e6338c8aa31ac64a
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 01:18:28 +0300

    Create identity-event.routes.ts

---
+++ backend/src/modules/identity/presentation/routes/identity-event.routes.ts
@@ -0,0 +1,8 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/presentation/routes/identity-event.routes.ts
+// NEW FILE
+// ============================================================================
+
+GET     /identity/events
+
+GET     /identity/events/timeline/{userId}

77922813504970c6b67ae995133242d59e7884f0
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 16:10:37 +0300

    Create risk-normalized.event.ts

---
+++ backend/src/modules/identity/domain/events/risk-normalized.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/domain/events/risk-normalized.event.ts
+// NEW FILE
+// ============================================================================
+
+export class RiskNormalizedEvent{
+
+    constructor(
+
+        readonly userId:string
+
+    ){}
+
+}

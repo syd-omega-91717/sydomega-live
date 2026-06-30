0c0bd23e501d1f921b3af9e874e4ed77a78a3886
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 09:22:13 +0300

    Create scim-users.controller.ts

---
+++ backend/src/modules/scim/presentation/controllers/scim-users.controller.ts
@@ -0,0 +1,20 @@
+// ============================================================================
+// FILE: /backend/src/modules/scim/presentation/controllers/scim-users.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class ScimUsersController {
+
+    async create(){}
+
+    async list(){}
+
+    async get(){}
+
+    async update(){}
+
+    async patch(){}
+
+    async delete(){}
+
+}

e4b1e00a29d45a2bf3f6104c78d483fd405b7194
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 00:17:10 +0300

    Create disconnect-session-stream.command.ts

---
+++ backend/src/modules/identity/application/commands/disconnect-session-stream.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/application/commands/disconnect-session-stream.command.ts
+// NEW FILE
+// ============================================================================
+
+export class DisconnectSessionStreamCommand{
+
+    constructor(
+
+        readonly sessionId:string
+
+    ){}
+
+}

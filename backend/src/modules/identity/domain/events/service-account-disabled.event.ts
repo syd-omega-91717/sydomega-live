eb8540caae17f921e94ed1004feb3576cd988ed9
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 15:40:58 +0300

    Create service-account-disabled.event.ts

---
+++ backend/src/modules/identity/domain/events/service-account-disabled.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/domain/events/service-account-disabled.event.ts
+// NEW FILE
+// ============================================================================
+
+export class ServiceAccountDisabledEvent{
+
+    constructor(
+
+        readonly serviceAccountId:string
+
+    ){}
+
+}

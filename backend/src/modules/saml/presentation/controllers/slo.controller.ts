9a1940737e26c7df1331bb9890ae792f7e0a92db
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 08:36:51 +0300

    Create slo.controller.ts

---
+++ backend/src/modules/saml/presentation/controllers/slo.controller.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/saml/presentation/controllers/slo.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class SloController {
+
+    async logout(){}
+
+}

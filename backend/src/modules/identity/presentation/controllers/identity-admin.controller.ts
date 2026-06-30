9aae3dd326e88c0fbe39f22baaee3c764bf6468e
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 00:01:57 +0300

    Create identity-admin.controller.ts

---
+++ backend/src/modules/identity/presentation/controllers/identity-admin.controller.ts
@@ -0,0 +1,24 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/presentation/controllers/identity-admin.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class IdentityAdminController{
+
+    async lockUser(){}
+
+    async unlockUser(){}
+
+    async suspendUser(){}
+
+    async activateUser(){}
+
+    async disableUser(){}
+
+    async revokeSessions(){}
+
+    async resetPassword(){}
+
+    async resetMfa(){}
+
+}

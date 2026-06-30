4aae8585002ea7f1686713692845db32e0c1aa39
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 09:22:45 +0300

    Create scim-groups.controller.ts

---
+++ backend/src/modules/scim/presentation/controllers/scim-groups.controller.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE: /backend/src/modules/scim/presentation/controllers/scim-groups.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class ScimGroupsController {
+
+    async create(){}
+
+    async list(){}
+
+    async update(){}
+
+    async patch(){}
+
+    async delete(){}
+
+}

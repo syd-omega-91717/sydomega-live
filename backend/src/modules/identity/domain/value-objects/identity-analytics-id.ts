5dfd56dd948ad202c9d14794ce4807120ced0883
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 22:41:27 +0300

    Create identity-analytics-id.ts

---
+++ backend/src/modules/identity/domain/value-objects/identity-analytics-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/domain/value-objects/identity-analytics-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class IdentityAnalyticsId
+extends UniqueEntityId {}

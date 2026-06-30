9feb5058e108288efcb623f27f7c2333d4dd0892
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 15:46:54 +0300

    Create service-account.controller.ts

---
+++ backend/src/modules/identity/presentation/controllers/service-account.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/presentation/controllers/service-account.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class ServiceAccountController{
+
+    async create(){}
+
+    async list(){}
+
+    async rotateSecret(){}
+
+    async disable(){}
+
+}

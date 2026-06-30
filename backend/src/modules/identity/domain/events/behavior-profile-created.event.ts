47ce19a39b362becde361d381116215eb5b2ea4b
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 22:27:56 +0300

    Create behavior-profile-created.event.ts

---
+++ backend/src/modules/identity/domain/events/behavior-profile-created.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/domain/events/behavior-profile-created.event.ts
+// NEW FILE
+// ============================================================================
+
+export class BehaviorProfileCreatedEvent{
+
+    constructor(
+
+        readonly userId:string
+
+    ){}
+
+}

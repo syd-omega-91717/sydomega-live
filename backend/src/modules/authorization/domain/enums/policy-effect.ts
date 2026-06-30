5ff5806e9dd6ce33a1c394e9eb562b1d1f70ad8b
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 02:20:29 +0300

    Create policy-effect.ts

---
+++ backend/src/modules/authorization/domain/enums/policy-effect.ts
@@ -0,0 +1,12 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/enums/policy-effect.ts
+// NEW FILE
+// ============================================================================
+
+export enum PolicyEffect{
+
+    Allow="ALLOW",
+
+    Deny="DENY"
+
+}

dcee72d95f7ce561b3a18209505731af7b6b90ce
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 16:07:20 +0300

    Create risk-level.ts

---
+++ backend/src/modules/identity/domain/enums/risk-level.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/domain/enums/risk-level.ts
+// NEW FILE
+// ============================================================================
+
+export enum RiskLevel{
+
+    Low="LOW",
+
+    Medium="MEDIUM",
+
+    High="HIGH",
+
+    Critical="CRITICAL"
+
+}

8e74be5dc54613c0399342851af97a564b175936
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 11:46:20 +0300

    Create api-key-secret.ts

---
+++ backend/src/modules/identity/domain/value-objects/api-key-secret.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/domain/value-objects/api-key-secret.ts
+// NEW FILE
+// ============================================================================
+
+export class ApiKeySecret {
+
+    constructor(
+
+        readonly hash: string
+
+    ){}
+
+}

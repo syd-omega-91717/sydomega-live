3913d29eb30ca0ef3a5c9baaefb99b315ca24864
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 01:17:51 +0300

    Create identity-event.controller.ts

---
+++ backend/src/modules/identity/presentation/controllers/identity-event.controller.ts
@@ -0,0 +1,12 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/presentation/controllers/identity-event.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class IdentityEventController{
+
+    async timeline(){}
+
+    async events(){}
+
+}

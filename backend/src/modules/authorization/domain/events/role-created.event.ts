90031f00f3e7d5d48d26fc9ca6e08d8b49515eef
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 02:36:44 +0300

    Create role-created.event.ts

---
+++ backend/src/modules/authorization/domain/events/role-created.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/role-created.event.ts
+// NEW FILE
+// ============================================================================
+
+export class RoleCreatedEvent{
+
+    constructor(
+
+        readonly roleId:string
+
+    ){}
+
+}

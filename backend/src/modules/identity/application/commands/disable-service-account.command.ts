f9ed1a932e2962c397d8b5a5ebeda105758a9ace
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 15:43:55 +0300

    Create disable-service-account.command.ts

---
+++ backend/src/modules/identity/application/commands/disable-service-account.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/application/commands/disable-service-account.command.ts
+// NEW FILE
+// ============================================================================
+
+export class DisableServiceAccountCommand{
+
+    constructor(
+
+        readonly serviceAccountId:string
+
+    ){}
+
+}

692a4c16a4f2534c4649d9b3ecaa015e05cee297
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 22:36:40 +0300

    Create behavior.controller.ts

---
+++ backend/src/modules/identity/presentation/controllers/behavior.controller.ts
@@ -0,0 +1,12 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/presentation/controllers/behavior.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class BehaviorController{
+
+    async analyze(){}
+
+    async profile(){}
+
+}

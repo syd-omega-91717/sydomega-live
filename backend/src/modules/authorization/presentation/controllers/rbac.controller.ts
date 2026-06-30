556bd1bf044df5828ab2ce174438b27f236ebb52
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 02:41:30 +0300

    Create rbac.controller.ts

---
+++ backend/src/modules/authorization/presentation/controllers/rbac.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/rbac.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class RbacController{
+
+    async roles(){}
+
+    async createRole(){}
+
+    async assignRole(){}
+
+    async revokeRole(){}
+
+}

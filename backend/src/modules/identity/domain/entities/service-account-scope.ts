7810d55f5b3a337010699e66e700d5b551dd2b02
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 15:35:16 +0300

    Create service-account-scope.ts

---
+++ backend/src/modules/identity/domain/entities/service-account-scope.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/domain/entities/service-account-scope.ts
+// NEW FILE
+// ============================================================================
+
+export class ServiceAccountScope {
+
+    constructor(
+
+        readonly value:string
+
+    ){}
+
+}

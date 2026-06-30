bccbe56438df1e442ec0e78aa21219988e90cbaf
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 01:23:12 +0300

    Create audit-severity.ts

---
+++ backend/src/modules/audit/domain/enums/audit-severity.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE: /backend/src/modules/audit/domain/enums/audit-severity.ts
+// NEW FILE
+// ============================================================================
+
+export enum AuditSeverity{
+
+    Information="INFORMATION",
+
+    Low="LOW",
+
+    Medium="MEDIUM",
+
+    High="HIGH",
+
+    Critical="CRITICAL"
+
+}

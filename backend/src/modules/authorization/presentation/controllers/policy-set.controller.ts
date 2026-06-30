1aee4ae6184930b8177e8c3fc61c8cb56a0f37ba
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 02:57:00 +0300

    Create policy-set.controller.ts

---
+++ backend/src/modules/authorization/presentation/controllers/policy-set.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/policy-set.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class PolicySetController{
+
+    async create(){}
+
+    async addRule(){}
+
+    async removeRule(){}
+
+    async evaluate(){}
+
+}

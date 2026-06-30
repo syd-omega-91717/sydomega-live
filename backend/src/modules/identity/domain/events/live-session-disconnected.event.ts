1257dee9e8d08105bf188a3c2b1491bc545b23dd
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 00:15:35 +0300

    Create live-session-disconnected.event.ts

---
+++ backend/src/modules/identity/domain/events/live-session-disconnected.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/domain/events/live-session-disconnected.event.ts
+// NEW FILE
+// ============================================================================
+
+export class LiveSessionDisconnectedEvent{
+
+    constructor(
+
+        readonly sessionId:string
+
+    ){}
+
+}

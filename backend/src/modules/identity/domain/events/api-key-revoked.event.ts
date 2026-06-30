b000b952e7b6728e3bdc90ef9c7e9f0fb11dd7b5
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 11:57:50 +0300

    Create api-key-revoked.event.ts

---
+++ backend/src/modules/identity/domain/events/api-key-revoked.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/domain/events/api-key-revoked.event.ts
+// NEW FILE
+// ============================================================================
+
+export class ApiKeyRevokedEvent {
+
+    constructor(
+
+        readonly apiKeyId:string
+
+    ){}
+
+}

177863759c798d0ff4c5c30acbea2a7855272d71
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 01:23:46 +0300

    Create audit-outcome.ts

---
+++ backend/src/modules/audit/domain/enums/audit-outcome.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/audit/domain/enums/audit-outcome.ts
+// NEW FILE
+// ============================================================================
+
+export enum AuditOutcome{
+
+    Success="SUCCESS",
+
+    Failure="FAILURE",
+
+    Denied="DENIED",
+
+    Error="ERROR"
+
+}

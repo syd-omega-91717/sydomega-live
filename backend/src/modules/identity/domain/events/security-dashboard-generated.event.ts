d4b77a9f505e80ce3612452f87c9686474bba0b5
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-06-30 22:45:16 +0300

    Create security-dashboard-generated.event.ts

---
+++ backend/src/modules/identity/domain/events/security-dashboard-generated.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/identity/domain/events/security-dashboard-generated.event.ts
+// NEW FILE
+// ============================================================================
+
+export class SecurityDashboardGeneratedEvent{
+
+    constructor(
+
+        readonly dashboardId:string
+
+    ){}
+
+}

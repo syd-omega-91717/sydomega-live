f5f94932eb5508563f642b54feebf27bd06e9b2d
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 19:01:38 +0300

    Add SodConflictResolvedEvent class

---
+++ backend/src/modules/authorization/domain/events/sod-conflict-resolved.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/sod-conflict-resolved.event.ts
+// NEW FILE
+// ============================================================================
+
+export class SodConflictResolvedEvent{
+
+    constructor(
+
+        readonly conflictId:string
+
+    ){}
+
+}

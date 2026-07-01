6f43c04b6b506793a699f59c4165928cd940bbce
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 19:48:48 +0300

    Add PrivilegedSessionStartedEvent class

---
+++ backend/src/modules/authorization/domain/events/privileged-session-started.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/privileged-session-started.event.ts
+// NEW FILE
+// ============================================================================
+
+export class PrivilegedSessionStartedEvent{
+
+    constructor(
+
+        readonly sessionId:string
+
+    ){}
+
+}

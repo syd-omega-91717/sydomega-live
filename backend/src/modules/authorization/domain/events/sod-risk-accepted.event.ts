b6a8af358c96c275a20d93ecc6bcf1160a6d8bab
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 19:02:07 +0300

    Add SodRiskAcceptedEvent class for risk acceptance

---
+++ backend/src/modules/authorization/domain/events/sod-risk-accepted.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/sod-risk-accepted.event.ts
+// NEW FILE
+// ============================================================================
+
+export class SodRiskAcceptedEvent{
+
+    constructor(
+
+        readonly conflictId:string
+
+    ){}
+
+}

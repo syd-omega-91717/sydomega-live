c0329a4a688b1a19198c00747ac9b4382f0918b0
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 03:06:04 +0300

    Add AttributeRegisteredEvent class for event handling

---
+++ backend/src/modules/authorization/domain/events/attribute-registered.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/attribute-registered.event.ts
+// NEW FILE
+// ============================================================================
+
+export class AttributeRegisteredEvent{
+
+    constructor(
+
+        readonly attributeId:string
+
+    ){}
+
+}

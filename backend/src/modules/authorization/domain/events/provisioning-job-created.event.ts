261bc9f62cf8f777984abfbe6ed613b3b206e242
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 17:10:16 +0300

    Add ProvisioningJobCreatedEvent class

---
+++ backend/src/modules/authorization/domain/events/provisioning-job-created.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/provisioning-job-created.event.ts
+// NEW FILE
+// ============================================================================
+
+export class ProvisioningJobCreatedEvent{
+
+    constructor(
+
+        readonly jobId:string
+
+    ){}
+
+}

6b26c381b5515de6d9e4726cd56cfcdd3815d788
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 00:04:14 +0300

    Add ApiKeyController with CRUD methods

---
+++ backend/src/modules/authorization/presentation/controllers/api-key.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/api-key.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class ApiKeyController{
+
+    async create(){}
+
+    async rotate(){}
+
+    async revoke(){}
+
+    async list(){}
+
+}

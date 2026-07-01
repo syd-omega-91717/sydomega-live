3479a59d3bdf417672a8e3a487be5ead3ef9086f
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 03:45:20 +0300

    Add AccessReviewController with CRUD methods

---
+++ backend/src/modules/authorization/presentation/controllers/access-review.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/access-review.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class AccessReviewController{
+
+    async create(){}
+
+    async complete(){}
+
+    async active(){}
+
+    async overdue(){}
+
+}

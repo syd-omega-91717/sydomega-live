daedfe0f3d4a5017c64b99c33abe714b5947e28a
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 16:50:55 +0300

    Add CreateAccessPackageCommand class

---
+++ backend/src/modules/authorization/application/commands/create-access-package.command.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/application/commands/create-access-package.command.ts
+// NEW FILE
+// ============================================================================
+
+export class CreateAccessPackageCommand{
+
+    constructor(
+
+        readonly name:string,
+
+        readonly description:string
+
+    ){}
+
+}

a2caa3074f847b41d6449cc9d92d818767daf8d3
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 17:48:13 +0300

    Add ConnectorOnlineEvent class for connector events

---
+++ backend/src/modules/authorization/domain/events/connector-online.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/connector-online.event.ts
+// NEW FILE
+// ============================================================================
+
+export class ConnectorOnlineEvent{
+
+    constructor(
+
+        readonly connectorId:string
+
+    ){}
+
+}

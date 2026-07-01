cf2af2bef86e25ec7157ae5d6f9db0a776899b77
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 16:41:46 +0300

    Add AccessPackageId value object

---
+++ backend/src/modules/authorization/domain/value-objects/access-package-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/value-objects/access-package-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class AccessPackageId
+extends UniqueEntityId {}

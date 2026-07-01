c1a0e6024fbf3486f30d04ea6e885344af9a0d68
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 00:22:16 +0300

    Add ServiceAccountController with basic methods

---
+++ backend/src/modules/authorization/presentation/controllers/service-account.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/service-account.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class ServiceAccountController{
+
+    async create(){}
+
+    async rotateSecret(){}
+
+    async disable(){}
+
+    async list(){}
+
+}

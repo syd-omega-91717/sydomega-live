19d8405c5fec9ac91595f4349f95719924d9c32f
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 20:22:03 +0300

    Add VaultProviderDisconnectedEvent class

---
+++ backend/src/modules/authorization/domain/events/vault-provider-disconnected.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/vault-provider-disconnected.event.ts
+// NEW FILE
+// ============================================================================
+
+export class VaultProviderDisconnectedEvent{
+
+    constructor(
+
+        readonly providerId:string
+
+    ){}
+
+}

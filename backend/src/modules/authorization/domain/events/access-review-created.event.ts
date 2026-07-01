c61d88a93b21f5905209336fe13ee7212e919ae1
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 03:41:04 +0300

    Add AccessReviewCreatedEvent class

---
+++ backend/src/modules/authorization/domain/events/access-review-created.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/access-review-created.event.ts
+// NEW FILE
+// ============================================================================
+
+export class AccessReviewCreatedEvent{
+
+    constructor(
+
+        readonly reviewId:string
+
+    ){}
+
+}

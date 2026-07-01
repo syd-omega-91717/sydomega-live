44f2b683a6540626c9d60731da51faed87de0fe4
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 20:25:42 +0300

    Add VaultProviderController with basic methods

---
+++ backend/src/modules/authorization/presentation/controllers/vault-provider.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/vault-provider.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class VaultProviderController{
+
+    async register(){}
+
+    async synchronize(){}
+
+    async list(){}
+
+    async disconnect(){}
+
+}

3d5ef50abf37c0186464995415550e9f608f03a9
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 20:07:07 +0300

    Add SecretCreatedEvent class for secret creation events

---
+++ backend/src/modules/authorization/domain/events/secret-created.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/secret-created.event.ts
+// NEW FILE
+// ============================================================================
+
+export class SecretCreatedEvent{
+
+    constructor(
+
+        readonly secretId:string
+
+    ){}
+
+}

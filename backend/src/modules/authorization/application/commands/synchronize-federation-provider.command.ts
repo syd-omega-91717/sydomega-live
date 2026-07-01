bb009fdc930c0572591a9907f09a49e1a7fd09ea
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 20:52:19 +0300

    Add SynchronizeFederationProviderCommand class

---
+++ backend/src/modules/authorization/application/commands/synchronize-federation-provider.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/application/commands/synchronize-federation-provider.command.ts
+// NEW FILE
+// ============================================================================
+
+export class SynchronizeFederationProviderCommand{
+
+    constructor(
+
+        readonly providerId:string
+
+    ){}
+
+}

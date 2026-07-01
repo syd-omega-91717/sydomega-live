26b830fba88648dbf9652010ad41d1a8fbb33f0e
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 17:49:39 +0300

    Add SynchronizeConnectorCommand class

---
+++ backend/src/modules/authorization/application/commands/synchronize-connector.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/application/commands/synchronize-connector.command.ts
+// NEW FILE
+// ============================================================================
+
+export class SynchronizeConnectorCommand{
+
+    constructor(
+
+        readonly connectorId:string
+
+    ){}
+
+}

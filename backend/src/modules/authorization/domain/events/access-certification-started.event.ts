0782b61b1915d25b013beb784e79533b523627b5
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 15:42:35 +0300

    Add AccessCertificationStartedEvent class

---
+++ backend/src/modules/authorization/domain/events/access-certification-started.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/access-certification-started.event.ts
+// NEW FILE
+// ============================================================================
+
+export class AccessCertificationStartedEvent{
+
+    constructor(
+
+        readonly certificationId:string
+
+    ){}
+
+}

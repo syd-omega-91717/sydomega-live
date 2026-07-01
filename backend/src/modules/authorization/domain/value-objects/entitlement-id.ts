e39bb25a0ece47f475718f38fdbea22321a06afb
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 16:05:37 +0300

    Create entitlement-id.ts

---
+++ backend/src/modules/authorization/domain/value-objects/entitlement-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/value-objects/entitlement-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class EntitlementId
+extends UniqueEntityId {}

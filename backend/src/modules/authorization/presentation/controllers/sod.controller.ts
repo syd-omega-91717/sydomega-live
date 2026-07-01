4ce1277ba328325ef1beecf913b6f7ae530b7c86
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 18:39:22 +0300

    Add SodController with basic async methods

---
+++ backend/src/modules/authorization/presentation/controllers/sod.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/sod.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class SodController{
+
+    async create(){}
+
+    async addRule(){}
+
+    async violations(){}
+
+    async validate(){}
+
+}

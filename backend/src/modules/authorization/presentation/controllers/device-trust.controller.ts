c1276df22797b7d3c423bfbe61c6d04219173f13
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 21:54:14 +0300

    Add DeviceTrustController with basic methods

---
+++ backend/src/modules/authorization/presentation/controllers/device-trust.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/device-trust.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class DeviceTrustController{
+
+    async register(){}
+
+    async verify(){}
+
+    async trusted(){}
+
+    async revoke(){}
+
+}

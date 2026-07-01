aea7950a1f042a75ce19622b131834c8eb103c18
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 18:55:17 +0300

    Create sod-conflict-id.ts

---
+++ backend/src/modules/authorization/domain/value-objects/sod-conflict-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/value-objects/sod-conflict-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class SodConflictId
+extends UniqueEntityId {}

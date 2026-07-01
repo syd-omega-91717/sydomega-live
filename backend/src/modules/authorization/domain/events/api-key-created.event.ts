43da4c3a9c87b68412041712fc2d98c93b7eb615
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 00:00:06 +0300

    Add ApiKeyCreatedEvent class for event handling

---
+++ backend/src/modules/authorization/domain/events/api-key-created.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/api-key-created.event.ts
+// NEW FILE
+// ============================================================================
+
+export class ApiKeyCreatedEvent{
+
+    constructor(
+
+        readonly apiKeyId:string
+
+    ){}
+
+}

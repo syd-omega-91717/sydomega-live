270f9a965b9ca43dc3253dfc35f9898bf97c91ed
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 22:22:20 +0300

    Create conditional-access-policy-id.ts

---
+++ backend/src/modules/authorization/domain/value-objects/conditional-access-policy-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/value-objects/conditional-access-policy-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class ConditionalAccessPolicyId
+extends UniqueEntityId {}

d4f3b5939458a5ccb32e175e19fb228a0e5f455d
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 19:36:23 +0300

    Add JitAccessController with basic methods

---
+++ backend/src/modules/authorization/presentation/controllers/jit-access.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/jit-access.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class JitAccessController{
+
+    async request(){}
+
+    async activate(){}
+
+    async revoke(){}
+
+    async active(){}
+
+}

f5489e724625e396eab98f397e44d723d25e8385
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 17:44:04 +0300

    Add ProvisioningConnectorId value object

---
+++ backend/src/modules/authorization/domain/value-objects/provisioning-connector-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/value-objects/provisioning-connector-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class ProvisioningConnectorId
+extends UniqueEntityId {}

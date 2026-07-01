62c69e50dd13a46f9ba929b5fc28eb1957e62247
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 15:48:14 +0300

    Add AccessCertificationController with initial methods

---
+++ backend/src/modules/authorization/presentation/controllers/access-certification.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/access-certification.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class AccessCertificationController{
+
+    async start(){}
+
+    async certify(){}
+
+    async active(){}
+
+    async overdue(){}
+
+}

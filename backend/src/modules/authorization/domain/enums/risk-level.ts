692d4ebb35da746bd84bc6b2241e89abe88877f9
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 19:12:31 +0300

    Create RiskLevel enum for authorization module

---
+++ backend/src/modules/authorization/domain/enums/risk-level.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/enums/risk-level.ts
+// NEW FILE
+// ============================================================================
+
+export enum RiskLevel{
+
+    None="NONE",
+
+    Low="LOW",
+
+    Medium="MEDIUM",
+
+    High="HIGH",
+
+    Critical="CRITICAL"
+
+}

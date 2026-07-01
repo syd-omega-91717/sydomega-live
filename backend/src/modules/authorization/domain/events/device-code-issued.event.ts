dd02b41a96e6124b9497dd3e6ee12f1a0c75f413
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 01:04:22 +0300

    Add DeviceCodeIssuedEvent class for event handling

---
+++ backend/src/modules/authorization/domain/events/device-code-issued.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/device-code-issued.event.ts
+// NEW FILE
+// ============================================================================
+
+export class DeviceCodeIssuedEvent{
+
+    constructor(
+
+        readonly deviceCodeId:string
+
+    ){}
+
+}

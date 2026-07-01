976e441983d89f58675b7266bd5b2c38fa2b0b92
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 20:50:49 +0300

    Add FederationProviderRegisteredEvent class

---
+++ backend/src/modules/authorization/domain/events/federation-provider-registered.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/federation-provider-registered.event.ts
+// NEW FILE
+// ============================================================================
+
+export class FederationProviderRegisteredEvent{
+
+    constructor(
+
+        readonly providerId:string
+
+    ){}
+
+}

8a322bc38564d070e2ba9e11e5c5fab1a39a8d7e
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 20:18:49 +0300

    Create vault-provider-id.ts

---
+++ backend/src/modules/authorization/domain/value-objects/vault-provider-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/value-objects/vault-provider-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class VaultProviderId
+extends UniqueEntityId {}

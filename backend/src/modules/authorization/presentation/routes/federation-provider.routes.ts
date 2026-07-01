1c686c25fcd54ad696209f3f880f94f6ae6f2d91
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 20:55:30 +0300

    Create federation-provider.routes.ts

---
+++ backend/src/modules/authorization/presentation/routes/federation-provider.routes.ts
@@ -0,0 +1,12 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/routes/federation-provider.routes.ts
+// NEW FILE
+// ============================================================================
+
+GET     /authorization/federation/providers
+
+POST    /authorization/federation/providers
+
+POST    /authorization/federation/providers/{id}/synchronize
+
+DELETE  /authorization/federation/providers/{id}

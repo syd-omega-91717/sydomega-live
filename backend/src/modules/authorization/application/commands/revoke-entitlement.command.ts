c885784a938d8884cc0a9716d2dd18562df7c8f9
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 16:12:59 +0300

    Add RevokeEntitlementCommand class

---
+++ backend/src/modules/authorization/application/commands/revoke-entitlement.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/application/commands/revoke-entitlement.command.ts
+// NEW FILE
+// ============================================================================
+
+export class RevokeEntitlementCommand{
+
+    constructor(
+
+        readonly entitlementId:string
+
+    ){}
+
+}

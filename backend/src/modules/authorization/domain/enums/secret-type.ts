5c9f060b3c8c02ffc5f246bc814286bc586c1318
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 20:04:33 +0300

    Create secret-type.ts

---
+++ backend/src/modules/authorization/domain/enums/secret-type.ts
@@ -0,0 +1,20 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/enums/secret-type.ts
+// NEW FILE
+// ============================================================================
+
+export enum SecretType{
+
+    Password="PASSWORD",
+
+    ApiKey="API_KEY",
+
+    Certificate="CERTIFICATE",
+
+    SshKey="SSH_KEY",
+
+    Token="TOKEN",
+
+    CloudCredential="CLOUD_CREDENTIAL"
+
+}

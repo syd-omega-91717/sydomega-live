9649738caf64edcb747cb227c123e2eb7e3b2341
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 19:37:03 +0300

    Create jit-access.routes.ts

---
+++ backend/src/modules/authorization/presentation/routes/jit-access.routes.ts
@@ -0,0 +1,12 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/routes/jit-access.routes.ts
+// NEW FILE
+// ============================================================================
+
+GET     /authorization/jit
+
+POST    /authorization/jit
+
+POST    /authorization/jit/{id}/activate
+
+DELETE  /authorization/jit/{id}

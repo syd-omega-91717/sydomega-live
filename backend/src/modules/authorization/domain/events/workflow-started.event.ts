ee9ac4d08b807f90ae35403e06b6b1d190e0ded9
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 04:03:07 +0300

    Add WorkflowStartedEvent class for workflow events

---
+++ backend/src/modules/authorization/domain/events/workflow-started.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/workflow-started.event.ts
+// NEW FILE
+// ============================================================================
+
+export class WorkflowStartedEvent{
+
+    constructor(
+
+        readonly workflowId:string
+
+    ){}
+
+}

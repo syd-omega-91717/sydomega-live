776c7bb18ee42ccda256906d5405cc8808904d24
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 00:33:00 +0300

    Add OAuthClientController with basic methods

---
+++ backend/src/modules/authorization/presentation/controllers/oauth-client.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/oauth-client.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class OAuthClientController{
+
+    async register(){}
+
+    async rotateSecret(){}
+
+    async disable(){}
+
+    async list(){}
+
+}

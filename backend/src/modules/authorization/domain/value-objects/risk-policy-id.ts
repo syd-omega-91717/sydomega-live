c09966cf13cb5402a81ed910ce61a8db3cff91aa
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 19:12:03 +0300

    Create risk-policy-id.ts

---
+++ backend/src/modules/authorization/domain/value-objects/risk-policy-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/value-objects/risk-policy-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class RiskPolicyId
+extends UniqueEntityId {}

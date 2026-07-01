68e8329c4b05eb674ebde2985f1036eb26dcb93c
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 19:31:37 +0300

    Add ActivateJitAccessCommand class

---
+++ backend/src/modules/authorization/application/commands/activate-jit-access.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/application/commands/activate-jit-access.command.ts
+// NEW FILE
+// ============================================================================
+
+export class ActivateJitAccessCommand{
+
+    constructor(
+
+        readonly jitAccessId:string
+
+    ){}
+
+}

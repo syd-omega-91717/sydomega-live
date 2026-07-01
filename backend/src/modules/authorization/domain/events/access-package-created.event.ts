e4c57844a8d61a24d62204c2ae82d6e324b8dea9
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 16:49:11 +0300

    Add AccessPackageCreatedEvent class

---
+++ backend/src/modules/authorization/domain/events/access-package-created.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/access-package-created.event.ts
+// NEW FILE
+// ============================================================================
+
+export class AccessPackageCreatedEvent{
+
+    constructor(
+
+        readonly packageId:string
+
+    ){}
+
+}

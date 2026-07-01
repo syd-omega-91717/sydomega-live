f8402175d2a3c4a02e4cfb747edbe68384e64ff1
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 18:07:14 +0300

    Add CreateSodPolicyCommand class for policy creation

---
+++ backend/src/modules/authorization/application/commands/create-sod-policy.command.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/application/commands/create-sod-policy.command.ts
+// NEW FILE
+// ============================================================================
+
+export class CreateSodPolicyCommand{
+
+    constructor(
+
+        readonly name:string,
+
+        readonly type:string
+
+    ){}
+
+}

35dafbac00d9940280fcddb2b20109efd0210aff
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 03:41:32 +0300

    Add AccessReviewCompletedEvent class

---
+++ backend/src/modules/authorization/domain/events/access-review-completed.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/access-review-completed.event.ts
+// NEW FILE
+// ============================================================================
+
+export class AccessReviewCompletedEvent{
+
+    constructor(
+
+        readonly reviewId:string
+
+    ){}
+
+}

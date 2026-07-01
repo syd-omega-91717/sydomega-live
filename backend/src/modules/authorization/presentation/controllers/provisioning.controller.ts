dc676f4da3dc3b81d488c6357c4aa339cabedba2
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 17:16:34 +0300

    Add ProvisioningController with CRUD methods

---
+++ backend/src/modules/authorization/presentation/controllers/provisioning.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/provisioning.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class ProvisioningController{
+
+    async create(){}
+
+    async status(){}
+
+    async retry(){}
+
+    async cancel(){}
+
+}

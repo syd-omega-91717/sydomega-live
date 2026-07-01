90307525317cb4cda7e9b21bbf8cd5c0d89067c2
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 03:42:39 +0300

    Add CompleteAccessReviewCommand class

---
+++ backend/src/modules/authorization/application/commands/complete-access-review.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/application/commands/complete-access-review.command.ts
+// NEW FILE
+// ============================================================================
+
+export class CompleteAccessReviewCommand{
+
+    constructor(
+
+        readonly reviewId:string
+
+    ){}
+
+}

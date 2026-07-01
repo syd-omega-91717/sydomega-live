6337f5db7f1d66cfd00c99200f9f4f581975025a
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 23:37:08 +0300

    Create access-token-id.ts

---
+++ backend/src/modules/authorization/domain/value-objects/access-token-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/value-objects/access-token-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class AccessTokenId
+extends UniqueEntityId {}

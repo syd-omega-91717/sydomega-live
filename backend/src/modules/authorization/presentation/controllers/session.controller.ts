ee8212062dcd7045e04dbcc9063c2d6da0e0b01d
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 23:29:09 +0300

    Add SessionController for session management

---
+++ backend/src/modules/authorization/presentation/controllers/session.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/session.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class SessionController{
+
+    async create(){}
+
+    async active(){}
+
+    async revoke(){}
+
+    async logout(){}
+
+}

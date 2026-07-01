fe35cff3b78a7ac3ebd8156a0aa01e6565a054b1
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 00:48:21 +0300

    Add AuthorizationCodeController with basic methods

---
+++ backend/src/modules/authorization/presentation/controllers/authorization-code.controller.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/authorization-code.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class AuthorizationCodeController{
+
+    async authorize(){}
+
+    async token(){}
+
+    async revoke(){}
+
+}

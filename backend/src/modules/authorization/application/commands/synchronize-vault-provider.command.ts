cdc5f06f3d986c12b50f026452b4523b47482f28
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 20:23:06 +0300

    Add SynchronizeVaultProviderCommand class

---
+++ backend/src/modules/authorization/application/commands/synchronize-vault-provider.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/application/commands/synchronize-vault-provider.command.ts
+// NEW FILE
+// ============================================================================
+
+export class SynchronizeVaultProviderCommand{
+
+    constructor(
+
+        readonly providerId:string
+
+    ){}
+
+}

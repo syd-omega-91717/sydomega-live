01aba8d2c7d30c1b11252f7e30c8a92e37b5e76f
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 03:10:46 +0300

    Add AbacController with attribute methods

---
+++ backend/src/modules/authorization/presentation/controllers/abac.controller.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/abac.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class AbacController{
+
+    async attributes(){}
+
+    async register(){}
+
+    async evaluate(){}
+
+}

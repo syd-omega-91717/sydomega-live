389132262cd800009496d13842c98e82b3174781
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 17:12:49 +0300

    Add StartProvisioningJobCommand class

---
+++ backend/src/modules/authorization/application/commands/start-provisioning-job.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/application/commands/start-provisioning-job.command.ts
+// NEW FILE
+// ============================================================================
+
+export class StartProvisioningJobCommand{
+
+    constructor(
+
+        readonly jobId:string
+
+    ){}
+
+}

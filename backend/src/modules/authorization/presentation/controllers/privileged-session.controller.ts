ad7646eb26b071bacb8a82207ece51f4cf131b64
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 19:55:27 +0300

    Create privileged-session.controller.ts

---
+++ backend/src/modules/authorization/presentation/controllers/privileged-session.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/privileged-session.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class PrivilegedSessionController{
+
+    async start(){}
+
+    async terminate(){}
+
+    async active(){}
+
+    async audit(){}
+
+}

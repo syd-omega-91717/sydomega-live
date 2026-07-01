c6c26eff2d7a5b9d994bb232d5ad16875a25234d
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 21:43:05 +0300

    Create device-trust-id.ts

---
+++ backend/src/modules/authorization/domain/value-objects/device-trust-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/value-objects/device-trust-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class DeviceTrustId
+extends UniqueEntityId {}

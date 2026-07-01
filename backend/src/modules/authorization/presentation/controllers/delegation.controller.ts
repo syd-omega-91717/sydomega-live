5723e4ee380d0b8e6bbe4b5f73b6dee28513dbed
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 03:35:19 +0300

    Add DelegationController with create, revoke, and active methods

---
+++ backend/src/modules/authorization/presentation/controllers/delegation.controller.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/delegation.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class DelegationController{
+
+    async create(){}
+
+    async revoke(){}
+
+    async active(){}
+
+}

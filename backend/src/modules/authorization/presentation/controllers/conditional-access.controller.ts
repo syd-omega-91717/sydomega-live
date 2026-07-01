ba98cc6f689a96daf3074e4c152eb76a44db3566
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 22:29:09 +0300

    Add ConditionalAccessController with basic methods

---
+++ backend/src/modules/authorization/presentation/controllers/conditional-access.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/conditional-access.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class ConditionalAccessController{
+
+    async create(){}
+
+    async evaluate(){}
+
+    async list(){}
+
+    async disable(){}
+
+}

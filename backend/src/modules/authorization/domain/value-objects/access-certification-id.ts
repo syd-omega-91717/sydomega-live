d46529a99c9cb43ba9bcd50d821596145d34b837
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 15:39:14 +0300

    Add AccessCertificationId value object

---
+++ backend/src/modules/authorization/domain/value-objects/access-certification-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/value-objects/access-certification-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class AccessCertificationId
+extends UniqueEntityId {}

4d899806569c185857b109e1cbcafad9ddac3c50
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 19:24:32 +0300

    Add JitAccessId value object

---
+++ backend/src/modules/authorization/domain/value-objects/jit-access-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/value-objects/jit-access-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class JitAccessId
+extends UniqueEntityId {}

26f646937be9e007f795feeba2a26d966782f486
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 21:46:56 +0300

    Add DeviceRegisteredEvent class for device registration

---
+++ backend/src/modules/authorization/domain/events/device-registered.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/device-registered.event.ts
+// NEW FILE
+// ============================================================================
+
+export class DeviceRegisteredEvent{
+
+    constructor(
+
+        readonly deviceId:string
+
+    ){}
+
+}

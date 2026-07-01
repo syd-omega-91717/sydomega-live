5e82692b1904a818c2b2d2f98442ed6c5cbf8ec6
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 16:49:45 +0300

    Add AccessPackagePublishedEvent class

---
+++ backend/src/modules/authorization/domain/events/access-package-published.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/access-package-published.event.ts
+// NEW FILE
+// ============================================================================
+
+export class AccessPackagePublishedEvent{
+
+    constructor(
+
+        readonly packageId:string
+
+    ){}
+
+}

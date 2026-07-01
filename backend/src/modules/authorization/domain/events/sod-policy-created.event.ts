ce6b72314994af678266290f438a2ccbf5d2a95e
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 18:02:58 +0300

    Add SodPolicyCreatedEvent class for policy creation

---
+++ backend/src/modules/authorization/domain/events/sod-policy-created.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/sod-policy-created.event.ts
+// NEW FILE
+// ============================================================================
+
+export class SodPolicyCreatedEvent{
+
+    constructor(
+
+        readonly policyId:string
+
+    ){}
+
+}

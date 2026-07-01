fc21e5c6c4afb2e057468b407405bd8e036a19cc
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 17:47:39 +0300

    Add ConnectorRegisteredEvent class for event handling

---
+++ backend/src/modules/authorization/domain/events/connector-registered.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/connector-registered.event.ts
+// NEW FILE
+// ============================================================================
+
+export class ConnectorRegisteredEvent{
+
+    constructor(
+
+        readonly connectorId:string
+
+    ){}
+
+}

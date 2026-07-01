1d911088952d3825fe2792049b11dcceae293ad6
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 19:39:25 +0300

    Create privileged-session-id.ts

---
+++ backend/src/modules/authorization/domain/value-objects/privileged-session-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/value-objects/privileged-session-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class PrivilegedSessionId
+extends UniqueEntityId {}

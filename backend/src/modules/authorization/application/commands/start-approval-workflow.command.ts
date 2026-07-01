0ed209b52f84919b198ede5bfaffcf894cf1cf78
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 04:04:41 +0300

    Add StartApprovalWorkflowCommand class

---
+++ backend/src/modules/authorization/application/commands/start-approval-workflow.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/application/commands/start-approval-workflow.command.ts
+// NEW FILE
+// ============================================================================
+
+export class StartApprovalWorkflowCommand{
+
+    constructor(
+
+        readonly requestId:string
+
+    ){}
+
+}

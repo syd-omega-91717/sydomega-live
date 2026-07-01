a453d41a13364ad32f80d2b4bc9d546dc909a09d
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 20:05:04 +0300

    Create secret-status.ts

---
+++ backend/src/modules/authorization/domain/enums/secret-status.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/enums/secret-status.ts
+// NEW FILE
+// ============================================================================
+
+export enum SecretStatus{
+
+    Active="ACTIVE",
+
+    CheckedOut="CHECKED_OUT",
+
+    Rotating="ROTATING",
+
+    Revoked="REVOKED",
+
+    Expired="EXPIRED"
+
+}

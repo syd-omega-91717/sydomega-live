72a5170d80763c7fcf384e331c6e47b934434b32
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 19:29:25 +0300

    Add JitAccessActivatedEvent class

---
+++ backend/src/modules/authorization/domain/events/jit-access-activated.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/jit-access-activated.event.ts
+// NEW FILE
+// ============================================================================
+
+export class JitAccessActivatedEvent{
+
+    constructor(
+
+        readonly jitAccessId:string
+
+    ){}
+
+}

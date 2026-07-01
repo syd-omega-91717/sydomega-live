fe24273126ce33ef28a97f174c95f13209c022e5
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 03:50:32 +0300

    Add AccessRequestSubmittedEvent class

---
+++ backend/src/modules/authorization/domain/events/access-request-submitted.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/access-request-submitted.event.ts
+// NEW FILE
+// ============================================================================
+
+export class AccessRequestSubmittedEvent{
+
+    constructor(
+
+        readonly requestId:string
+
+    ){}
+
+}

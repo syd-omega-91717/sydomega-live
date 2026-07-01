df3bcaa203559a30f50faceff752ad5b6ba2fd00
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 03:47:38 +0300

    Add AccessRequestId value object

---
+++ backend/src/modules/authorization/domain/value-objects/access-request-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/value-objects/access-request-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class AccessRequestId
+extends UniqueEntityId {}

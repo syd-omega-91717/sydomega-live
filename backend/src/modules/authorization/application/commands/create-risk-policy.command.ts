047c9097b19bd6fad486fe906b04c212741639a5
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 19:17:36 +0300

    Add CreateRiskPolicyCommand class

---
+++ backend/src/modules/authorization/application/commands/create-risk-policy.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/application/commands/create-risk-policy.command.ts
+// NEW FILE
+// ============================================================================
+
+export class CreateRiskPolicyCommand{
+
+    constructor(
+
+        readonly name:string
+
+    ){}
+
+}

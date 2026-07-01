3683ae9160e0e0b8799224477414b803de11d3ba
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 23:44:24 +0300

    Add TokenController with methods for token management

---
+++ backend/src/modules/authorization/presentation/controllers/token.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/token.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class TokenController{
+
+    async issue(){}
+
+    async revoke(){}
+
+    async introspect(){}
+
+    async refresh(){}
+
+}

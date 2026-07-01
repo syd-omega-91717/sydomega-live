6875158a3304aae9df6b167d7dd207fdd0be2735
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 16:10:55 +0300

    Add EntitlementRevokedEvent class for event handling

---
+++ backend/src/modules/authorization/domain/events/entitlement-revoked.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/entitlement-revoked.event.ts
+// NEW FILE
+// ============================================================================
+
+export class EntitlementRevokedEvent{
+
+    constructor(
+
+        readonly entitlementId:string
+
+    ){}
+
+}

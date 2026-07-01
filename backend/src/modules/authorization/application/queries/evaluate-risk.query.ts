34ff6d6224c996623b3ce69d3a3c1b0ef785be46
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 19:18:37 +0300

    Add EvaluateRiskQuery class for risk evaluation

---
+++ backend/src/modules/authorization/application/queries/evaluate-risk.query.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/application/queries/evaluate-risk.query.ts
+// NEW FILE
+// ============================================================================
+
+export class EvaluateRiskQuery{
+
+    constructor(
+
+        readonly principalId:string
+
+    ){}
+
+}

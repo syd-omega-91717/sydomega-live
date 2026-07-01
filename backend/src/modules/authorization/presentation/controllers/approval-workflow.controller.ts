c2ba7caea0d3c206c2b417734ad0cb9023e99417
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 04:07:52 +0300

    Add ApprovalWorkflowController with basic methods

---
+++ backend/src/modules/authorization/presentation/controllers/approval-workflow.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/approval-workflow.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class ApprovalWorkflowController{
+
+    async start(){}
+
+    async approve(){}
+
+    async reject(){}
+
+    async active(){}
+
+}

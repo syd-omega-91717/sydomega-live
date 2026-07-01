c2531f4f51d1427f65e5eacf312c71732aab2f3d
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 17:53:46 +0300

    Add ProvisioningConnectorController with basic methods

---
+++ backend/src/modules/authorization/presentation/controllers/provisioning-connector.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/provisioning-connector.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class ProvisioningConnectorController{
+
+    async register(){}
+
+    async synchronize(){}
+
+    async health(){}
+
+    async list(){}
+
+}

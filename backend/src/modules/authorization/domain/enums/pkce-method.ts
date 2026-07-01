9e93687f9d260f87d2189a31861ed6f941ff7102
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 00:41:23 +0300

    Add PkceMethod enum for PKCE methods

---
+++ backend/src/modules/authorization/domain/enums/pkce-method.ts
@@ -0,0 +1,12 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/enums/pkce-method.ts
+// NEW FILE
+// ============================================================================
+
+export enum PkceMethod{
+
+    Plain="PLAIN",
+
+    S256="S256"
+
+}

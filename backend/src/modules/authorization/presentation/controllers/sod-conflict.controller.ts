d7484cd8feff812479bf01054cec992ebf65f499
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 19:05:46 +0300

    Add SodConflictController with basic methods

---
+++ backend/src/modules/authorization/presentation/controllers/sod-conflict.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/sod-conflict.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class SodConflictController{
+
+    async open(){}
+
+    async mitigate(){}
+
+    async acceptRisk(){}
+
+    async resolve(){}
+
+}

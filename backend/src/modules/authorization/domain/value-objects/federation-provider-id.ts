cb83a44203a7d121950167a60378f9dd187fad51
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 20:39:18 +0300

    Add FederationProviderId value object

---
+++ backend/src/modules/authorization/domain/value-objects/federation-provider-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/value-objects/federation-provider-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class FederationProviderId
+extends UniqueEntityId {}

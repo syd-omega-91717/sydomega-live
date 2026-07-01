1456c56323c4a02f34bda0b8e46d70dd593e6592
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 20:21:28 +0300

    Add VaultProviderConnectedEvent class

---
+++ backend/src/modules/authorization/domain/events/vault-provider-connected.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/vault-provider-connected.event.ts
+// NEW FILE
+// ============================================================================
+
+export class VaultProviderConnectedEvent{
+
+    constructor(
+
+        readonly providerId:string
+
+    ){}
+
+}

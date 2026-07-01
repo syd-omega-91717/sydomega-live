70bfd59146bac1ee037fecf3ab4304f3895d7f2c
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 16:54:58 +0300

    Add AccessPackageController with basic methods

---
+++ backend/src/modules/authorization/presentation/controllers/access-package.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/access-package.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class AccessPackageController{
+
+    async catalog(){}
+
+    async create(){}
+
+    async publish(){}
+
+    async assign(){}
+
+}

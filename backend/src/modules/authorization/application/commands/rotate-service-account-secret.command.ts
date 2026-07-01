a3a21381cb6eda5a5257aadf91b8f986c7b0a7ad
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 00:19:39 +0300

    Add RotateServiceAccountSecretCommand class

---
+++ backend/src/modules/authorization/application/commands/rotate-service-account-secret.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/application/commands/rotate-service-account-secret.command.ts
+// NEW FILE
+// ============================================================================
+
+export class RotateServiceAccountSecretCommand{
+
+    constructor(
+
+        readonly serviceAccountId:string
+
+    ){}
+
+}

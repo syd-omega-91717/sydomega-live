5fd3b95ebf513cd641339c7d53c1ddada933eaac
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 17:48:41 +0300

    Add ConnectorOfflineEvent class for event handling

---
+++ backend/src/modules/authorization/domain/events/connector-offline.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/connector-offline.event.ts
+// NEW FILE
+// ============================================================================
+
+export class ConnectorOfflineEvent{
+
+    constructor(
+
+        readonly connectorId:string
+
+    ){}
+
+}

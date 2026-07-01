f9e263029a9dc042e0464b9937056cae681100c8
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 20:14:33 +0300

    Add SecretController with basic async methods

---
+++ backend/src/modules/authorization/presentation/controllers/secret.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/secret.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class SecretController{
+
+    async create(){}
+
+    async checkout(){}
+
+    async rotate(){}
+
+    async revoke(){}
+
+}

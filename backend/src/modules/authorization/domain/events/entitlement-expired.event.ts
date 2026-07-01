ac3a3cec8812b93f88704c019ffb5565fb26fdda
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 16:11:45 +0300

    Add EntitlementExpiredEvent class

---
+++ backend/src/modules/authorization/domain/events/entitlement-expired.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/entitlement-expired.event.ts
+// NEW FILE
+// ============================================================================
+
+export class EntitlementExpiredEvent{
+
+    constructor(
+
+        readonly entitlementId:string
+
+    ){}
+
+}

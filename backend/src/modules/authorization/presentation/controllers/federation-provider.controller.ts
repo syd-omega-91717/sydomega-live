1eed7f8a2135234aca2c8464b7a4598f4e5f10e5
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 20:55:01 +0300

    Add FederationProviderController with basic methods

---
+++ backend/src/modules/authorization/presentation/controllers/federation-provider.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/federation-provider.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class FederationProviderController{
+
+    async register(){}
+
+    async synchronize(){}
+
+    async list(){}
+
+    async disable(){}
+
+}

477cd848908639c4652eb91b27ba770d83b00bf1
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 20:47:42 +0300

    Create federation-provider-status.ts

---
+++ backend/src/modules/authorization/domain/enums/federation-provider-status.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/enums/federation-provider-status.ts
+// NEW FILE
+// ============================================================================
+
+export enum FederationProviderStatus{
+
+    Active="ACTIVE",
+
+    Disabled="DISABLED",
+
+    Synchronizing="SYNCHRONIZING",
+
+    Error="ERROR"
+
+}

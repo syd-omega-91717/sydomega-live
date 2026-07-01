f45869426e06548172ba4b7faffcbbd0139bdc37
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 20:51:18 +0300

    Add FederationSynchronizationCompletedEvent class

---
+++ backend/src/modules/authorization/domain/events/federation-synchronization-completed.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/federation-synchronization-completed.event.ts
+// NEW FILE
+// ============================================================================
+
+export class FederationSynchronizationCompletedEvent{
+
+    constructor(
+
+        readonly providerId:string
+
+    ){}
+
+}

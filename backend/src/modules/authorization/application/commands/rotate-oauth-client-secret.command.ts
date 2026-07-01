12d307654229bc218ef403e60053a9b741c2accd
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 00:30:33 +0300

    Add RotateOAuthClientSecretCommand class

---
+++ backend/src/modules/authorization/application/commands/rotate-oauth-client-secret.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/application/commands/rotate-oauth-client-secret.command.ts
+// NEW FILE
+// ============================================================================
+
+export class RotateOAuthClientSecretCommand{
+
+    constructor(
+
+        readonly clientId:string
+
+    ){}
+
+}

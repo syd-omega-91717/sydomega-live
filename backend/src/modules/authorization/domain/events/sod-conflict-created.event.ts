d16cfc1709fbde6a4fe29a1def535df9cc90c247
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-01 19:01:07 +0300

    Add SodConflictCreatedEvent class for conflict handling

---
+++ backend/src/modules/authorization/domain/events/sod-conflict-created.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/sod-conflict-created.event.ts
+// NEW FILE
+// ============================================================================
+
+export class SodConflictCreatedEvent{
+
+    constructor(
+
+        readonly conflictId:string
+
+    ){}
+
+}

6f5a8265ecfaae62706693eb93b8024d95a1b5cb
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 00:16:41 +0300

    Create service-account-created.event.ts

---
+++ backend/src/modules/authorization/domain/events/service-account-created.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/service-account-created.event.ts
+// NEW FILE
+// ============================================================================
+
+export class ServiceAccountCreatedEvent{
+
+    constructor(
+
+        readonly serviceAccountId:string
+
+    ){}
+
+}

7e28658c16beeb8804e303f2c93a6b80cf049fae
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 00:40:53 +0300

    Create authorization-code-id.ts

---
+++ backend/src/modules/authorization/domain/value-objects/authorization-code-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/value-objects/authorization-code-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class AuthorizationCodeId
+extends UniqueEntityId {}

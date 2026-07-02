1e682697fccf75e82a63716308137916bfa26742
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 02:42:41 +0300

    Add AgentController with basic async methods

---
+++ backend/src/modules/ai-agents/presentation/controllers/agent.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/ai-agents/presentation/controllers/agent.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class AgentController{
+
+    async register(){}
+
+    async assign(){}
+
+    async execute(){}
+
+    async agents(){}
+
+}

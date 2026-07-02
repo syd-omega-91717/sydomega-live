db9413e730851cf8282bbb67b10c2dc4dc297ac9
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 08:45:20 +0300

    Add GenerateJwkCommand class for JWK generation

---
+++ backend/src/modules/authorization/application/commands/generate-jwk.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/application/commands/generate-jwk.command.ts
+// NEW FILE
+// ============================================================================
+
+export class GenerateJwkCommand{
+
+    constructor(
+
+        readonly algorithm:string
+
+    ){}
+
+}

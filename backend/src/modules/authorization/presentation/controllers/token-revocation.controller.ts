cda455e6c1b717c85c8ebaeb653318601dcc0b25
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 08:31:14 +0300

    Add TokenRevocationController for token revocation

---
+++ backend/src/modules/authorization/presentation/controllers/token-revocation.controller.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/token-revocation.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class TokenRevocationController{
+
+    async revoke(){}
+
+}

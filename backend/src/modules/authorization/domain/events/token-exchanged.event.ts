5a8904904910cdefea07dcf2e6045f474d578084
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 08:21:49 +0300

    Add TokenExchangedEvent class for token exchange events

---
+++ backend/src/modules/authorization/domain/events/token-exchanged.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/events/token-exchanged.event.ts
+// NEW FILE
+// ============================================================================
+
+export class TokenExchangedEvent{
+
+    constructor(
+
+        readonly exchangeId:string
+
+    ){}
+
+}

cf8cdaf9372e438ed3c9382df5c1ed14124a82d5
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 23:41:48 +0300

    Create rag-session-id.ts

---
+++ backend/src/modules/ai/domain/value-objects/rag-session-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/ai/domain/value-objects/rag-session-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class RagSessionId
+extends UniqueEntityId {}

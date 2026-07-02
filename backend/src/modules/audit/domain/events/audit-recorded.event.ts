795a869114d59ad7b1b899b6518757972f264514
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 09:23:49 +0300

    Create audit-recorded.event.ts

---
+++ backend/src/modules/audit/domain/events/audit-recorded.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/audit/domain/events/audit-recorded.event.ts
+// NEW FILE
+// ============================================================================
+
+export class AuditRecordedEvent{
+
+    constructor(
+
+        readonly auditRecordId:string
+
+    ){}
+
+}

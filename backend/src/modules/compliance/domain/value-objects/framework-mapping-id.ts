59fa2819ef67cfd1fd5bb4878378aa9e2a1cdb8c
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 21:16:16 +0300

    Create framework-mapping-id.ts

---
+++ backend/src/modules/compliance/domain/value-objects/framework-mapping-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/compliance/domain/value-objects/framework-mapping-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class FrameworkMappingId
+extends UniqueEntityId {}

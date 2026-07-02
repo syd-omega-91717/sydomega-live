3adebc9897a07778ee60fd7f31befbd2cf197b52
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 08:30:08 +0300

    Add RevokeOAuthTokenCommand class

---
+++ backend/src/modules/authorization/application/commands/revoke-oauth-token.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/application/commands/revoke-oauth-token.command.ts
+// NEW FILE
+// ============================================================================
+
+export class RevokeOAuthTokenCommand{
+
+    constructor(
+
+        readonly token:string
+
+    ){}
+
+}

83a50c416dd04085e84ef7894b46ff4da56ea630
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 22:32:13 +0300

    Add ReportController with methods for reports

---
+++ backend/src/modules/compliance/presentation/controllers/report.controller.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE: /backend/src/modules/compliance/presentation/controllers/report.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class ReportController{
+
+    async executive(){}
+
+    async board(){}
+
+    async regulatory(){}
+
+    async audit(){}
+
+    async export(){}
+
+}

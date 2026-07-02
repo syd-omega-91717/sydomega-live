4dbc14284adb4254a740cd2f57fd3090acf8a939
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 23:12:03 +0300

    Add OrchestrationStartedEvent class

---
+++ backend/src/modules/ai/domain/events/orchestration-started.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/ai/domain/events/orchestration-started.event.ts
+// NEW FILE
+// ============================================================================
+
+export class OrchestrationStartedEvent{
+
+    constructor(
+
+        readonly orchestrationId:string
+
+    ){}
+
+}

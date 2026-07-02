ca2c29415f46f812a91897044743012d808be65e
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 12:10:53 +0300

    Add VerifyHashChainCommand class

---
+++ backend/src/modules/audit/application/commands/verify-hash-chain.command.ts
@@ -0,0 +1,6 @@
+// ============================================================================
+// FILE: /backend/src/modules/audit/application/commands/verify-hash-chain.command.ts
+// NEW FILE
+// ============================================================================
+
+export class VerifyHashChainCommand{}

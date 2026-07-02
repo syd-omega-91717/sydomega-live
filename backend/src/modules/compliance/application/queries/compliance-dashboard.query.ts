32a95e30450393c17393b95f60b4be02ec98f442
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 22:05:26 +0300

    Add ComplianceDashboardQuery class for organization ID

---
+++ backend/src/modules/compliance/application/queries/compliance-dashboard.query.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/compliance/application/queries/compliance-dashboard.query.ts
+// NEW FILE
+// ============================================================================
+
+export class ComplianceDashboardQuery{
+
+    constructor(
+
+        readonly organizationId:string
+
+    ){}
+
+}

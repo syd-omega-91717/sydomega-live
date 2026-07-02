a6112fec3b93b2d0a5a6d9f1c3af5988dc86326b
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 22:19:51 +0300

    Add IssueCertificationCommand class

---
+++ backend/src/modules/compliance/application/commands/issue-certification.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/compliance/application/commands/issue-certification.command.ts
+// NEW FILE
+// ============================================================================
+
+export class IssueCertificationCommand{
+
+    constructor(
+
+        readonly certificationId:string
+
+    ){}
+
+}

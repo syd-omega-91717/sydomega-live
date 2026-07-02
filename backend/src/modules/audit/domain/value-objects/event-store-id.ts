ca9beaef7c418a701b56937ba8143da6891a2523
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 11:58:20 +0300

    Create event-store-id.ts

---
+++ backend/src/modules/audit/domain/value-objects/event-store-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/audit/domain/value-objects/event-store-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class EventStoreId
+extends UniqueEntityId {}

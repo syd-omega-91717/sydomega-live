5c46adf3b4aa40074476e5b4b63c458730eaf770
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 08:48:16 +0300

    Add JwksController with JWKS related methods

---
+++ backend/src/modules/authorization/presentation/controllers/jwks.controller.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/jwks.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class JwksController{
+
+    async jwks(){}
+
+    async rotate(){}
+
+    async current(){}
+
+}

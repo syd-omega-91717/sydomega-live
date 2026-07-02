94bb4f65694828f621644736a07326f861d684a5
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 22:37:15 +0300

    Create ai-request-id.ts

---
+++ backend/src/modules/ai/domain/value-objects/ai-request-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/ai/domain/value-objects/ai-request-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class AIRequestId
+extends UniqueEntityId {}

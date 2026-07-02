31c0e3995e4f578bfe7b6f0b34a9d077db201585
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 23:52:24 +0300

    Create vector-distance.ts

---
+++ backend/src/modules/ai/domain/enums/vector-distance.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/ai/domain/enums/vector-distance.ts
+// NEW FILE
+// ============================================================================
+
+export enum VectorDistance{
+
+    Cosine="COSINE",
+
+    DotProduct="DOT_PRODUCT",
+
+    Euclidean="EUCLIDEAN"
+
+}

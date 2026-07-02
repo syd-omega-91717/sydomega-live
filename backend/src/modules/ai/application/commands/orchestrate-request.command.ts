6b98c0de96016d9a895bd75b74a1f802f8b0d224
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 23:12:57 +0300

    Add OrchestrateRequestCommand class

---
+++ backend/src/modules/ai/application/commands/orchestrate-request.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/ai/application/commands/orchestrate-request.command.ts
+// NEW FILE
+// ============================================================================
+
+export class OrchestrateRequestCommand{
+
+    constructor(
+
+        readonly requestId:string
+
+    ){}
+
+}

46b343032b7b741f1a6a495f71e6711be60d24b1
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 15:57:09 +0300

    Add ComplianceAlertRaisedEvent class

---
+++ backend/src/modules/compliance/domain/events/compliance-alert-raised.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/compliance/domain/events/compliance-alert-raised.event.ts
+// NEW FILE
+// ============================================================================
+
+export class ComplianceAlertRaisedEvent{
+
+    constructor(
+
+        readonly alertId:string
+
+    ){}
+
+}

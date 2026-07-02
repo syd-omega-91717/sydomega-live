536b32dbc76232ca9e41d36a58a79662c6d9d588
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 22:18:56 +0300

    Add CertificationIssuedEvent class

---
+++ backend/src/modules/compliance/domain/events/certification-issued.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/compliance/domain/events/certification-issued.event.ts
+// NEW FILE
+// ============================================================================
+
+export class CertificationIssuedEvent{
+
+    constructor(
+
+        readonly certificationId:string
+
+    ){}
+
+}

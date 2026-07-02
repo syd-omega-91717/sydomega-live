37e98a57afa19fbab9de3aff158711c344e84055
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 12:04:45 +0300

    Add EventStoreController with basic methods

---
+++ backend/src/modules/audit/presentation/controllers/event-store.controller.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/audit/presentation/controllers/event-store.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class EventStoreController{
+
+    async append(){}
+
+    async replay(){}
+
+    async stream(){}
+
+}

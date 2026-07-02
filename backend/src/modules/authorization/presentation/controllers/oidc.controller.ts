a07f0d35594ddd95e9c2e2b900472fc83949c095
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 08:57:35 +0300

    Create oidc.controller.ts

---
+++ backend/src/modules/authorization/presentation/controllers/oidc.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/oidc.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class OidcController{
+
+    async discovery(){}
+
+    async userInfo(){}
+
+    async logout(){}
+
+    async endSession(){}
+
+}

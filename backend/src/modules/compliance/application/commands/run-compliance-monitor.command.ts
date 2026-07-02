a151c1eb486e8ccd22309e597788208889a41f87
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 15:57:41 +0300

    Add RunComplianceMonitorCommand class

---
+++ backend/src/modules/compliance/application/commands/run-compliance-monitor.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/compliance/application/commands/run-compliance-monitor.command.ts
+// NEW FILE
+// ============================================================================
+
+export class RunComplianceMonitorCommand{
+
+    constructor(
+
+        readonly resourceId:string
+
+    ){}
+
+}

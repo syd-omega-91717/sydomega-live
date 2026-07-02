36bcfc51978302f922a2855d2a64d7f8d132e94a
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 08:29:00 +0300

    Add TokenIntrospectionController with introspect method

---
+++ backend/src/modules/authorization/presentation/controllers/token-introspection.controller.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/controllers/token-introspection.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class TokenIntrospectionController{
+
+    async introspect(){}
+
+}

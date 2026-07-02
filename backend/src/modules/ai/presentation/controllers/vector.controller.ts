e24e4fd5b7c93b3a5e1d6d53d9b47ed2e4f7c570
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 00:01:22 +0300

    Add VectorController with basic async methods

---
+++ backend/src/modules/ai/presentation/controllers/vector.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/ai/presentation/controllers/vector.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class VectorController{
+
+    async indexes(){}
+
+    async index(){}
+
+    async search(){}
+
+    async optimize(){}
+
+}

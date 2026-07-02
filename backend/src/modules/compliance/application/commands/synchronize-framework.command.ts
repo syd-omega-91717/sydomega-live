e9d8a2aa459124599210ded918d371784da366bf
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 21:29:48 +0300

    Add SynchronizeFrameworkCommand class

---
+++ backend/src/modules/compliance/application/commands/synchronize-framework.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/compliance/application/commands/synchronize-framework.command.ts
+// NEW FILE
+// ============================================================================
+
+export class SynchronizeFrameworkCommand{
+
+    constructor(
+
+        readonly framework:string
+
+    ){}
+
+}

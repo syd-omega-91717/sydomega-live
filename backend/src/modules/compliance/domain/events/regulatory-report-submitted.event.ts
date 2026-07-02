e3dc71b832da27a75bb75801a0038120d028f4cd
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 22:28:59 +0300

    Create RegulatoryReportSubmittedEvent class

---
+++ backend/src/modules/compliance/domain/events/regulatory-report-submitted.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/compliance/domain/events/regulatory-report-submitted.event.ts
+// NEW FILE
+// ============================================================================
+
+export class RegulatoryReportSubmittedEvent{
+
+    constructor(
+
+        readonly submissionId:string
+
+    ){}
+
+}

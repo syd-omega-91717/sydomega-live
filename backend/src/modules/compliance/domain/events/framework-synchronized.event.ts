3e7fb29b770226f35ea4146faac4e59600b139ef
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 21:28:49 +0300

    Add FrameworkSynchronizedEvent class

---
+++ backend/src/modules/compliance/domain/events/framework-synchronized.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/compliance/domain/events/framework-synchronized.event.ts
+// NEW FILE
+// ============================================================================
+
+export class FrameworkSynchronizedEvent{
+
+    constructor(
+
+        readonly framework:string
+
+    ){}
+
+}

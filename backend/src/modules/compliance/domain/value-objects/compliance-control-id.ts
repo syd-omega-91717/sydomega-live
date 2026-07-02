db798aaa9d9421fda0671303e62d8cc62f6ca048
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 12:53:29 +0300

    Add ComplianceControlId value object

---
+++ backend/src/modules/compliance/domain/value-objects/compliance-control-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/compliance/domain/value-objects/compliance-control-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class ComplianceControlId
+extends UniqueEntityId {}

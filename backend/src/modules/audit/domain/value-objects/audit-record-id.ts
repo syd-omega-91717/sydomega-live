339372a78c86c91a01a1bb5b2ec9f6951c30796b
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 09:05:41 +0300

    Create audit-record-id.ts

---
+++ backend/src/modules/audit/domain/value-objects/audit-record-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/audit/domain/value-objects/audit-record-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class AuditRecordId
+extends UniqueEntityId {}

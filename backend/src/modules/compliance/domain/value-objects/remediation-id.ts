d0daab887486f12142f2a1464ce0c7c171ea5de2
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 16:32:00 +0300

    Create remediation-id.ts

---
+++ backend/src/modules/compliance/domain/value-objects/remediation-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/compliance/domain/value-objects/remediation-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class RemediationId
+extends UniqueEntityId {}

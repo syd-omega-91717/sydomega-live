fb6401e1993aeaf1b4a2d4f2c126646a60c9f52b
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 22:10:29 +0300

    Add CertificationId value object

---
+++ backend/src/modules/compliance/domain/value-objects/certification-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/compliance/domain/value-objects/certification-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class CertificationId
+extends UniqueEntityId {}

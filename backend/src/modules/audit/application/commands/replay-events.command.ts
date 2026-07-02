13d1a79885a00b72cf9e20c02ef212760d068b5c
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 12:02:26 +0300

    Add ReplayEventsCommand class for event replaying

---
+++ backend/src/modules/audit/application/commands/replay-events.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/audit/application/commands/replay-events.command.ts
+// NEW FILE
+// ============================================================================
+
+export class ReplayEventsCommand{
+
+    constructor(
+
+        readonly aggregateId:string
+
+    ){}
+
+}

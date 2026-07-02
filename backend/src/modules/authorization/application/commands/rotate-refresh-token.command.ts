8dc7d6bc666e614fa5748206b2f033e4a16587cf
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 08:08:18 +0300

    Add RotateRefreshTokenCommand class

---
+++ backend/src/modules/authorization/application/commands/rotate-refresh-token.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/application/commands/rotate-refresh-token.command.ts
+// NEW FILE
+// ============================================================================
+
+export class RotateRefreshTokenCommand{
+
+    constructor(
+
+        readonly refreshToken:string
+
+    ){}
+
+}

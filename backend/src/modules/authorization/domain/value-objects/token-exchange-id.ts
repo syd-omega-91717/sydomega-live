996959efbac2cea96bab7226974c46a697d0320f
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 08:19:30 +0300

    Add TokenExchangeId value object

---
+++ backend/src/modules/authorization/domain/value-objects/token-exchange-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/value-objects/token-exchange-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class TokenExchangeId
+extends UniqueEntityId {}

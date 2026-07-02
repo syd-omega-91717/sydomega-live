16710eaf89b1619198670e41c69b3cc20f261721
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 08:53:30 +0300

    Add OpenIdDiscoveryQuery class in new file

---
+++ backend/src/modules/authorization/application/queries/openid-discovery.query.ts
@@ -0,0 +1,6 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/application/queries/openid-discovery.query.ts
+// NEW FILE
+// ============================================================================
+
+export class OpenIdDiscoveryQuery{}

afa9d1afbb60d287912ff2705b668844f758d2e0
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 08:04:11 +0300

    Add RefreshTokenId value object

---
+++ backend/src/modules/authorization/domain/value-objects/refresh-token-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/domain/value-objects/refresh-token-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class RefreshTokenId
+extends UniqueEntityId {}

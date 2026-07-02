385c5d225af0ad12c348fc8525786f911d390496
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 02:59:20 +0300

    Add WorkflowEngineController with basic methods

---
+++ backend/src/modules/workflow/presentation/controllers/workflow-engine.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/workflow/presentation/controllers/workflow-engine.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class WorkflowEngineController{
+
+    async start(){}
+
+    async resume(){}
+
+    async cancel(){}
+
+    async executions(){}
+
+}

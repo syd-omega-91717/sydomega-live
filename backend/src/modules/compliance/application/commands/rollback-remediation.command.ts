92fb228c4f92de17396b549c0ce661a70fddff30
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 16:37:35 +0300

    Add RollbackRemediationCommand class

---
+++ backend/src/modules/compliance/application/commands/rollback-remediation.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/compliance/application/commands/rollback-remediation.command.ts
+// NEW FILE
+// ============================================================================
+
+export class RollbackRemediationCommand{
+
+    constructor(
+
+        readonly executionId:string
+
+    ){}
+
+}

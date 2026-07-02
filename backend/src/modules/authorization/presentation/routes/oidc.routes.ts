6ccd009c160b94d22eb6344b506b62894d9bb479
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 08:58:22 +0300

    Create oidc.routes.ts

---
+++ backend/src/modules/authorization/presentation/routes/oidc.routes.ts
@@ -0,0 +1,12 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/presentation/routes/oidc.routes.ts
+// NEW FILE
+// ============================================================================
+
+GET     /.well-known/openid-configuration
+
+GET     /oidc/userinfo
+
+GET     /oidc/logout
+
+POST    /oidc/end-session

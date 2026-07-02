1a29b0994b68239e394d3d8a7fd0c4955cc0fd71
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 12:10:25 +0300

    Add AppendHashChainCommand class

---
+++ backend/src/modules/audit/application/commands/append-hash-chain.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/audit/application/commands/append-hash-chain.command.ts
+// NEW FILE
+// ============================================================================
+
+export class AppendHashChainCommand{
+
+    constructor(
+
+        readonly payloadHash:string
+
+    ){}
+
+}

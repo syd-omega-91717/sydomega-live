8d5edc1f6b0806333c027d19c87fe8fe0ac9f27a
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 12:43:31 +0300

    Add VerifySignatureCommand class

---
+++ backend/src/modules/audit/application/commands/verify-signature.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/audit/application/commands/verify-signature.command.ts
+// NEW FILE
+// ============================================================================
+
+export class VerifySignatureCommand{
+
+    constructor(
+
+        readonly signatureId:string
+
+    ){}
+
+}

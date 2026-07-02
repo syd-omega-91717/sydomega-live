23ac0668f04f69658861fc8ff8db5b123d53e835
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 12:39:56 +0300

    Create digital-signature-id.ts

---
+++ backend/src/modules/audit/domain/value-objects/digital-signature-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/audit/domain/value-objects/digital-signature-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class DigitalSignatureId
+extends UniqueEntityId {}

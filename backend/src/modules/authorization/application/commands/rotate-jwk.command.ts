696ef4b6487fba2bfec3ab04e15cbd26a6dffc93
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-02 08:45:47 +0300

    Add RotateJwkCommand class for key rotation

---
+++ backend/src/modules/authorization/application/commands/rotate-jwk.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/authorization/application/commands/rotate-jwk.command.ts
+// NEW FILE
+// ============================================================================
+
+export class RotateJwkCommand{
+
+    constructor(
+
+        readonly keyId:string
+
+    ){}
+
+}

7ed2b44750830347b70504bf50e776614dbf9b33
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-04 01:50:01 +0300

    Create GISPage component to render GISMap

---
+++ frontend/app/gis/page.tsx
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE:
+// /frontend/app/gis/page.tsx
+// ============================================================================
+
+'use client';
+
+import GISMap from "@/features/gis/components/GISMap";
+
+export default function GISPage(){
+
+    return <GISMap/>;
+
+}

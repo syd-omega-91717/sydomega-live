47043442b547c988c019a1040a27fc9c72708e3d
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 10:27:18 +0300

    Create memory-id.ts

---
+++ backend/src/modules/memory/domain/value-objects/memory-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/memory/domain/value-objects/memory-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class MemoryId
+extends UniqueEntityId {}

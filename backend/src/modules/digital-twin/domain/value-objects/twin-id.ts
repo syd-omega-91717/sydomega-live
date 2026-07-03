7625f2980c1bb83d597e6f7b33ea7aafc3a3ce9d
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 20:17:08 +0300

    Create twin-id.ts

---
+++ backend/src/modules/digital-twin/domain/value-objects/twin-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/digital-twin/domain/value-objects/twin-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class TwinId
+extends UniqueEntityId {}

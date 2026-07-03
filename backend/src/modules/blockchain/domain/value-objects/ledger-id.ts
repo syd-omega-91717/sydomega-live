c1e995f74e01fa6c3481c1d17d1c1e40bb2118f2
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 21:46:46 +0300

    Add LedgerId class extending UniqueEntityId

---
+++ backend/src/modules/blockchain/domain/value-objects/ledger-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/blockchain/domain/value-objects/ledger-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class LedgerId
+extends UniqueEntityId {}

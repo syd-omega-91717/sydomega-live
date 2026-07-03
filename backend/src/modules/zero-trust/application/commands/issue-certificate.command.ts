b5fff5b4c3ac25410ccb35c77a25a8640b17b8f1
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 18:31:01 +0300

    Add IssueCertificateCommand class for certificate issuance

---
+++ backend/src/modules/zero-trust/application/commands/issue-certificate.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/zero-trust/application/commands/issue-certificate.command.ts
+// NEW FILE
+// ============================================================================
+
+export class IssueCertificateCommand{
+
+    constructor(
+
+        readonly workloadId:string
+
+    ){}
+
+}

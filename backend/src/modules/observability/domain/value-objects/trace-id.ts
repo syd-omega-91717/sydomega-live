e44120a2e4f7a3bc17869120de9486131059df10
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 14:11:10 +0300

    Create trace-id.ts

---
+++ backend/src/modules/observability/domain/value-objects/trace-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/observability/domain/value-objects/trace-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class TraceId
+extends UniqueEntityId {}

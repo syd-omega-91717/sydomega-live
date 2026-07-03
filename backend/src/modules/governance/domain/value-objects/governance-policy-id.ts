c19b1321997a86b68b4176ee7cd624a0af52c704
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 15:30:25 +0300

    Create governance-policy-id.ts

---
+++ backend/src/modules/governance/domain/value-objects/governance-policy-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/governance/domain/value-objects/governance-policy-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class GovernancePolicyId
+extends UniqueEntityId {}

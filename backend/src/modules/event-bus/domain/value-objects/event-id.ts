df6f0c7b2d03637aac8502b4a0140200c6856b7b
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 18:48:52 +0300

    Add EventId value object extending UniqueEntityId

---
+++ backend/src/modules/event-bus/domain/value-objects/event-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/event-bus/domain/value-objects/event-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class EventId
+extends UniqueEntityId {}

aab3869eb613fd725168995028b534d7a49c36f3
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 19:08:46 +0300

    Add CacheHitEvent class for cache hit events

---
+++ backend/src/modules/cache/domain/events/cache-hit.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/cache/domain/events/cache-hit.event.ts
+// NEW FILE
+// ============================================================================
+
+export class CacheHitEvent{
+
+    constructor(
+
+        readonly key:string
+
+    ){}
+
+}

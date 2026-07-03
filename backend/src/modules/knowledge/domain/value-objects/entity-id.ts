ad945710dfe1df437ec25790af42ab2175d2c5cd
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 13:28:41 +0300

    Create entity-id.ts

---
+++ backend/src/modules/knowledge/domain/value-objects/entity-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/knowledge/domain/value-objects/entity-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class EntityId
+extends UniqueEntityId {}

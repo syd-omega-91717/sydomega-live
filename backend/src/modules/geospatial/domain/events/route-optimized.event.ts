abe23e5f23df6ec96dd40369ead638d70cef73d1
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 20:34:49 +0300

    Add RouteOptimizedEvent class for route optimization

---
+++ backend/src/modules/geospatial/domain/events/route-optimized.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/geospatial/domain/events/route-optimized.event.ts
+// NEW FILE
+// ============================================================================
+
+export class RouteOptimizedEvent{
+
+    constructor(
+
+        readonly routeId:string
+
+    ){}
+
+}

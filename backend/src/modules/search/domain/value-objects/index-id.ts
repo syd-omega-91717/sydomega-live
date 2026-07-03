a1be2880988870c34e7f7b41305667dbe290cce6
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 19:47:26 +0300

    Create index-id.ts

---
+++ backend/src/modules/search/domain/value-objects/index-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/search/domain/value-objects/index-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class IndexId
+extends UniqueEntityId {}

40eb5af3196175dc15c73e6ebb7e1d299b1e1598
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-04 02:04:41 +0300

    Create WalletDashboard.tsx

---
+++ frontend/features/blockchain/components/WalletDashboard.tsx
@@ -0,0 +1,22 @@
+// ============================================================================
+// FILE:
+// /frontend/features/blockchain/components/WalletDashboard.tsx
+// ============================================================================
+
+'use client';
+
+import Card from "@/components/ui/Card";
+
+export default function WalletDashboard(){
+
+    return(
+
+        <Card title="Enterprise Wallet">
+
+            Wallet list
+
+        </Card>
+
+    );
+
+}

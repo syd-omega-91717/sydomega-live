2126f6fecad2743cb134d2f4b2bc95dcfcc91909
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-04 01:56:01 +0300

    Create TwinWorkspace.tsx

---
+++ frontend/features/digitalTwin/components/TwinWorkspace.tsx
@@ -0,0 +1,34 @@
+// ============================================================================
+// FILE:
+// /frontend/features/digitalTwin/components/TwinWorkspace.tsx
+// ============================================================================
+
+'use client';
+
+import Card from "@/components/ui/Card";
+
+export default function TwinWorkspace(){
+
+    return(
+
+        <Card title="Digital Twin">
+
+            <div
+                style={{
+
+                    height:700,
+
+                    border:"1px solid #ccc"
+
+                }}
+            >
+
+                3D Scene Placeholder
+
+            </div>
+
+        </Card>
+
+    );
+
+}

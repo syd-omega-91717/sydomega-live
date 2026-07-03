cc49dd31559dbe3a0bc5bd433e15953aa22f073f
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 03:09:47 +0300

    Add AutonomyController with mission management methods

---
+++ backend/src/modules/autonomy/presentation/controllers/autonomy.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/autonomy/presentation/controllers/autonomy.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class AutonomyController{
+
+    async createMission(){}
+
+    async replan(){}
+
+    async terminate(){}
+
+    async missions(){}
+
+}

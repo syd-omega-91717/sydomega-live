27a79379ed0db83b735c820b42fdfe8424fad9cb
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-04 01:48:54 +0300

    Create page.tsx

---
+++ frontend/app/ai/page.tsx
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /frontend/app/ai/page.tsx
+// ============================================================================
+
+'use client';
+
+import AIWorkspace from "@/features/ai/components/AIWorkspace";
+
+export default function AIPage(){
+
+    return (
+
+        <AIWorkspace/>
+
+    );
+
+}

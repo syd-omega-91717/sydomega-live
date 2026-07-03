832c8caf976e1b8884cec690b9dd2826ca5687a1
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 21:54:50 +0300

    Add BlockchainController with empty methods

---
+++ backend/src/modules/blockchain/presentation/controllers/blockchain.controller.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE: /backend/src/modules/blockchain/presentation/controllers/blockchain.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class BlockchainController{
+
+    async wallets(){}
+
+    async contracts(){}
+
+    async transactions(){}
+
+    async assets(){}
+
+    async bridge(){}
+
+}

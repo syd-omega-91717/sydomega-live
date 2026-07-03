9b981c7a0ebd846fa42c71c774841a8599344502
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 14:21:39 +0300

    Create budget-id.ts

---
+++ backend/src/modules/cost/domain/value-objects/budget-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/cost/domain/value-objects/budget-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class BudgetId
+extends UniqueEntityId {}

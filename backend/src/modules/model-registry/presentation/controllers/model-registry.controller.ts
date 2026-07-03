bc71ce3ac18b4ed1a8e9728946b02eafe4a6f08d
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 15:10:41 +0300

    Add ModelRegistryController with basic methods

---
+++ backend/src/modules/model-registry/presentation/controllers/model-registry.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/model-registry/presentation/controllers/model-registry.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class ModelRegistryController{
+
+    async register(){}
+
+    async deploy(){}
+
+    async rollback(){}
+
+    async catalog(){}
+
+}

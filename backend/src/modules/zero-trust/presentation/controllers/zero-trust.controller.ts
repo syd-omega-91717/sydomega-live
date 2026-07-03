4c55b764f7c6c9d5c18014457208a8fd78ee6355
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 18:33:15 +0300

    Add ZeroTrustController with empty methods

---
+++ backend/src/modules/zero-trust/presentation/controllers/zero-trust.controller.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE: /backend/src/modules/zero-trust/presentation/controllers/zero-trust.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class ZeroTrustController{
+
+    async policies(){}
+
+    async devices(){}
+
+    async workloads(){}
+
+    async sessions(){}
+
+    async certificates(){}
+
+}

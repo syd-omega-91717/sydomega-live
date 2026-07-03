aa6a92e199048b3d66098d36edab6445a05eac30
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 13:54:42 +0300

    Add ToolController with basic async methods

---
+++ backend/src/modules/tools/presentation/controllers/tool.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/tools/presentation/controllers/tool.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class ToolController{
+
+    async register(){}
+
+    async execute(){}
+
+    async discover(){}
+
+    async executions(){}
+
+}

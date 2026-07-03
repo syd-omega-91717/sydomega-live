9102bc41a0571716f9d2a2382e688ab2fba4fb93
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 22:16:14 +0300

    Add OrganizationAggregate class with basic methods

---
+++ backend/src/modules/organization/domain/organization.aggregate.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /backend/src/modules/organization/domain/organization.aggregate.ts
+// ============================================================================
+
+export class OrganizationAggregate{
+
+    createOrganization(){}
+
+    createTenant(){}
+
+    createBusinessUnit(){}
+
+    assignAdministrator(){}
+
+    manageHierarchy(){}
+
+}

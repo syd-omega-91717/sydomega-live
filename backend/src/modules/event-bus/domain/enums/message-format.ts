6fd18e0a26804e8fc83108be7ce8a3ed6a20328b
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 18:49:50 +0300

    Add MessageFormat enum for message formats

---
+++ backend/src/modules/event-bus/domain/enums/message-format.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/event-bus/domain/enums/message-format.ts
+// NEW FILE
+// ============================================================================
+
+export enum MessageFormat{
+
+    Protobuf="PROTOBUF",
+
+    Avro="AVRO",
+
+    JSON="JSON"
+
+}

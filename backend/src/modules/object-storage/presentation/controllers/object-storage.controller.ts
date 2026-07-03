c526d5e59f390f92f4048b0dbc5d5a16a8d60645
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 19:40:00 +0300

    Add ObjectStorageController with basic methods

---
+++ backend/src/modules/object-storage/presentation/controllers/object-storage.controller.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE: /backend/src/modules/object-storage/presentation/controllers/object-storage.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class ObjectStorageController{
+
+    async upload(){}
+
+    async download(){}
+
+    async buckets(){}
+
+    async versions(){}
+
+    async presignedUrls(){}
+
+}

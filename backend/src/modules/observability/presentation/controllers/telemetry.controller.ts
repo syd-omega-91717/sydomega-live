dfb9a76de2980723b475757b7fa16a25f888d8c7
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 14:18:16 +0300

    Add TelemetryController with async methods

---
+++ backend/src/modules/observability/presentation/controllers/telemetry.controller.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/observability/presentation/controllers/telemetry.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class TelemetryController{
+
+    async traces(){}
+
+    async metrics(){}
+
+    async dashboards(){}
+
+    async sla(){}
+
+}

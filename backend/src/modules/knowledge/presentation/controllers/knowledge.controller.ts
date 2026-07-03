48e5934eb3e46638159f8a2e5ca057d4181b00b9
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 13:35:50 +0300

    Add KnowledgeController with initial methods

---
+++ backend/src/modules/knowledge/presentation/controllers/knowledge.controller.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE: /backend/src/modules/knowledge/presentation/controllers/knowledge.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class KnowledgeController{
+
+    async entities(){}
+
+    async create(){}
+
+    async connect(){}
+
+    async traverse(){}
+
+    async infer(){}
+
+}

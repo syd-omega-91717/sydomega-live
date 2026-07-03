f6a66729b9254a07728a0037bf1c44f658ab9b01
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 18:16:23 +0300

    Create security-policy-id.ts

---
+++ backend/src/modules/zero-trust/domain/value-objects/security-policy-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/zero-trust/domain/value-objects/security-policy-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class SecurityPolicyId
+extends UniqueEntityId {}

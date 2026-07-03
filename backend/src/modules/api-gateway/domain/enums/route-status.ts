d180f6838ffe5ba27e6fa31684ffb9fa86c46886
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 16:21:31 +0300

    Create route-status.ts

---
+++ backend/src/modules/api-gateway/domain/enums/route-status.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/api-gateway/domain/enums/route-status.ts
+// NEW FILE
+// ============================================================================
+
+export enum RouteStatus{
+
+    Active="ACTIVE",
+
+    Disabled="DISABLED",
+
+    Deprecated="DEPRECATED"
+
+}

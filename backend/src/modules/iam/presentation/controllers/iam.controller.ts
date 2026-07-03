dade554eac96c54df759f6dc71049ed955025924
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 18:44:41 +0300

    Add IAMController with basic async methods

---
+++ backend/src/modules/iam/presentation/controllers/iam.controller.ts
@@ -0,0 +1,20 @@
+// ============================================================================
+// FILE: /backend/src/modules/iam/presentation/controllers/iam.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class IAMController{
+
+    async login(){}
+
+    async logout(){}
+
+    async organizations(){}
+
+    async users(){}
+
+    async roles(){}
+
+    async policies(){}
+
+}

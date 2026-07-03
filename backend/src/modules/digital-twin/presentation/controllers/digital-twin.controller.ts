e2a48e8f92dfbc14652ca3667ddffdf0abeb095b
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 20:25:00 +0300

    Add DigitalTwinController with initial methods

---
+++ backend/src/modules/digital-twin/presentation/controllers/digital-twin.controller.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE: /backend/src/modules/digital-twin/presentation/controllers/digital-twin.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class DigitalTwinController{
+
+    async twins(){}
+
+    async synchronize(){}
+
+    async simulate(){}
+
+    async replay(){}
+
+    async analytics(){}
+
+}

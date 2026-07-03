dc01979a9a0f05e29089207b991d120b03cef65b
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 19:10:16 +0300

    Add CacheInvalidateCommand class for cache invalidation

---
+++ backend/src/modules/cache/application/commands/cache-invalidate.command.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/cache/application/commands/cache-invalidate.command.ts
+// NEW FILE
+// ============================================================================
+
+export class CacheInvalidateCommand{
+
+    constructor(
+
+        readonly key:string
+
+    ){}
+
+}

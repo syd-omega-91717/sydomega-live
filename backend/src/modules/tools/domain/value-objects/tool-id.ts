400e6227bea8b70384641b81bbba51c857dfd5ca
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 13:45:14 +0300

    Create tool-id.ts

---
+++ backend/src/modules/tools/domain/value-objects/tool-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/tools/domain/value-objects/tool-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class ToolId
+extends UniqueEntityId {}

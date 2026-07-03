1e16cc0821a52bab3b970e79c840ca8a87a1a169
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 19:09:16 +0300

    Add CacheMissEvent class for cache miss events

---
+++ backend/src/modules/cache/domain/events/cache-miss.event.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE: /backend/src/modules/cache/domain/events/cache-miss.event.ts
+// NEW FILE
+// ============================================================================
+
+export class CacheMissEvent{
+
+    constructor(
+
+        readonly key:string
+
+    ){}
+
+}

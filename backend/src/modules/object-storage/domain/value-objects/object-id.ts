07aec10c978e724352b3f22a7368928755a9d1cd
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 19:31:52 +0300

    Add ObjectId class extending UniqueEntityId

---
+++ backend/src/modules/object-storage/domain/value-objects/object-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/object-storage/domain/value-objects/object-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class ObjectId
+extends UniqueEntityId {}

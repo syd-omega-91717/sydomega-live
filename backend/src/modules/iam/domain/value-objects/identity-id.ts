3a8ab8af80d92e83882ce94fcb3ca0bb4219c10c
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 18:36:58 +0300

    Add IdentityId class extending UniqueEntityId

---
+++ backend/src/modules/iam/domain/value-objects/identity-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/iam/domain/value-objects/identity-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class IdentityId
+extends UniqueEntityId {}

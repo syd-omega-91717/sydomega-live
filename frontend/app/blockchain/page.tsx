5939b7828371ef13ad6a4cbd1578a1d302a68648
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-04 02:02:18 +0300

    Create page.tsx

---
+++ frontend/app/blockchain/page.tsx
@@ -0,0 +1,15 @@
+// ============================================================================
+// FILE:
+// /frontend/app/blockchain/page.tsx
+// ============================================================================
+
+'use client';
+
+import WalletDashboard
+from "@/features/blockchain/components/WalletDashboard";
+
+export default function BlockchainPage(){
+
+    return <WalletDashboard/>;
+
+}

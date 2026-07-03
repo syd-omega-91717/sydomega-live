807ecb47f239f2484a3836d63d8335c8ab74bd18
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 20:31:27 +0300

    Add Geofence class with properties and constructor

---
+++ backend/src/modules/geospatial/domain/entities/geofence.ts
@@ -0,0 +1,19 @@
+// ========backend/src/modules/geospatial/domain/entities/geofence.ts
+// NEW FILE
+// ============================================================================
+
+export class Geofence{
+
+    constructor(
+
+        readonly geofenceId:string,
+
+        readonly name:string,
+
+        readonly polygon:string,
+
+        readonly enabled:boolean
+
+    ){}
+
+}

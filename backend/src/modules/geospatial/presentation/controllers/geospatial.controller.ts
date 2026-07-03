3320c9db59aa6d61903af9efe15bcd0db008993b
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 20:38:13 +0300

    Add GeospatialController with placeholder methods

---
+++ backend/src/modules/geospatial/presentation/controllers/geospatial.controller.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE: /backend/src/modules/geospatial/presentation/controllers/geospatial.controller.ts
+// NEW FILE
+// ============================================================================
+
+export class GeospatialController{
+
+    async layers(){}
+
+    async geofences(){}
+
+    async routes(){}
+
+    async heatmaps(){}
+
+    async tracking(){}
+
+}

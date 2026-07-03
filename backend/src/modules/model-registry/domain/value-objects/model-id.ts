66df58cb2fd6629467dd900271f2e0a03a9a6d1e
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 14:59:52 +0300

    Create model-id.ts

---
+++ backend/src/modules/model-registry/domain/value-objects/model-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/model-registry/domain/value-objects/model-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class ModelId
+extends UniqueEntityId {}

7df5b183b9a558f351423e41cad6d51732f9706b
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 13:51:32 +0300

    Add RegisterToolCommand class for tool registration

---
+++ backend/src/modules/tools/application/commands/register-tool.command.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE: /backend/src/modules/tools/application/commands/register-tool.command.ts
+// NEW FILE
+// ============================================================================
+
+export class RegisterToolCommand{
+
+    constructor(
+
+        readonly name:string,
+
+        readonly type:string
+
+    ){}
+
+}

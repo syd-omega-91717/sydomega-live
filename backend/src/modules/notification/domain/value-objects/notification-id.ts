bf124965027410f9ee83db209001c8451a5d77b0
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 19:19:55 +0300

    Add NotificationId value object

---
+++ backend/src/modules/notification/domain/value-objects/notification-id.ts
@@ -0,0 +1,10 @@
+// ============================================================================
+// FILE: /backend/src/modules/notification/domain/value-objects/notification-id.ts
+// NEW FILE
+// ============================================================================
+
+import { UniqueEntityId }
+from "@/kernel/domain/unique-entity-id";
+
+export class NotificationId
+extends UniqueEntityId {}

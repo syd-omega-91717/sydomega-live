c9e6296dabb0f12bbcab5133cae521bc87c7075e
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-03 22:19:55 +0300

    Create AgentMeshAggregate class with basic methods

---
+++ backend/src/modules/sovereign/domain/agent-mesh.aggregate.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /backend/src/modules/sovereign/domain/agent-mesh.aggregate.ts
+// ============================================================================
+
+export class AgentMeshAggregate{
+
+    registerAgent(){}
+
+    assignMission(){}
+
+    collaborate(){}
+
+    escalateDecision(){}
+
+    retireAgent(){}
+
+}

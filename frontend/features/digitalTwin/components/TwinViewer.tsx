b35104e92a0511c74967e0b3d0179f71e43ee429
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-04 19:18:36 +0300

    Create TwinViewer.tsx

---
+++ frontend/features/digitalTwin/components/TwinViewer.tsx
@@ -0,0 +1,24 @@
+// ============================================================================
+// FILE:
+// /frontend/features/digitalTwin/components/TwinViewer.tsx
+// ============================================================================
+
+'use client';
+
+export default function TwinViewer(){
+
+    return(
+
+        <div
+            style={{
+                height:650,
+                border:"1px solid gray"
+            }}>
+
+            3D DIGITAL TWIN VIEWPORT
+
+        </div>
+
+    );
+
+}

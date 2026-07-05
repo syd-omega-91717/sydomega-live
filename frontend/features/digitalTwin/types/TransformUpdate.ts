60e9f2c39c9740ce71f11377aa6914df90c33965
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-05 12:13:57 +0300

    Add TransformUpdate interface for 3D transformations

---
+++ frontend/features/digitalTwin/types/TransformUpdate.ts
@@ -0,0 +1,22 @@
+// ============================================================================
+// FILE:
+// /frontend/features/digitalTwin/types/TransformUpdate.ts
+// ============================================================================
+
+export interface TransformUpdate{
+
+    id:string;
+
+    x:number;
+
+    y:number;
+
+    z:number;
+
+    rx:number;
+
+    ry:number;
+
+    rz:number;
+
+}

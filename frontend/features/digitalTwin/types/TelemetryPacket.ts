7c4461aa9a6f7d50ebe0ff4345f645be28a03fa7
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-05 12:13:29 +0300

    Add TelemetryPacket interface definition

---
+++ frontend/features/digitalTwin/types/TelemetryPacket.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE:
+// /frontend/features/digitalTwin/types/TelemetryPacket.ts
+// ============================================================================
+
+export interface TelemetryPacket{
+
+    assetId:string;
+
+    timestamp:string;
+
+    value:number;
+
+    unit:string;
+
+}

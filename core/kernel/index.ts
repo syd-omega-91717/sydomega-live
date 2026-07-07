61088d9baa40c53c3e8fc0d2bd0d15b51da721c3
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 01:00:39 +0300

    Create index.ts to export kernel modules

---
+++ core/kernel/index.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE:
+// /core/kernel/index.ts
+// ============================================================================
+
+export * from "./Kernel";
+export * from "./MatrixBootstrap";
+export * from "./MatrixConstants";
+export * from "./MatrixContext";
+export * from "./MatrixCoordinate";
+export * from "./MatrixMetadata";
+export * from "./MatrixRegistry";
+export * from "./MatrixRuntime";
+export * from "./MatrixVersion";

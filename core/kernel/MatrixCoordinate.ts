307ee044cfbe7438927e26bbebdcaacf2314d6c2
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 00:57:45 +0300

    Add MatrixCoordinate interface definition

---
+++ core/kernel/MatrixCoordinate.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /core/kernel/MatrixCoordinate.ts
+// ============================================================================
+
+export interface MatrixCoordinate{
+
+    zodiac:number;
+
+    house:number;
+
+    governance:number;
+
+    fortress:number;
+
+    manifestation:number;
+
+}

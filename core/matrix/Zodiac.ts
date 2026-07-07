37ebae907f87c8abd80075dd94b20cb2e823619f
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 01:19:19 +0300

    Add Zodiac enum with astrological signs

---
+++ core/matrix/Zodiac.ts
@@ -0,0 +1,33 @@
+// ============================================================================
+// FOUNDATION KERNEL FK-002
+// FILE:
+// /core/matrix/Zodiac.ts
+// ============================================================================
+
+export enum Zodiac{
+
+    ARIES=1,
+
+    TAURUS,
+
+    GEMINI,
+
+    CANCER,
+
+    LEO,
+
+    VIRGO,
+
+    LIBRA,
+
+    SCORPIO,
+
+    SAGITTARIUS,
+
+    CAPRICORN,
+
+    AQUARIUS,
+
+    PISCES
+
+}

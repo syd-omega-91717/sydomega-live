a47d947d73a417ceff5ae17df9c7ff9f5fff3e92
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 01:25:34 +0300

    Create index.ts to export matrix modules

---
+++ core/matrix/index.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE:
+// /core/matrix/index.ts
+// ============================================================================
+
+export * from "./House";
+export * from "./MatrixClassifier";
+export * from "./MatrixCoordinateEngine";
+export * from "./MatrixEngine";
+export * from "./MatrixGrid";
+export * from "./MatrixIndex";
+export * from "./MatrixNavigator";
+export * from "./MatrixNode";
+export * from "./MatrixResolver";
+export * from "./MatrixTraversal";
+export * from "./Zodiac";

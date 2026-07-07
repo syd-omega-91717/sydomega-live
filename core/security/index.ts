8812d870c1111532723e585943a7b3f3811faad1
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 02:14:10 +0300

    Add index.ts for security module exports

---
+++ core/security/index.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /core/security/index.ts
+// ============================================================================
+
+export * from "./ConfigurationValidationShield";
+export * from "./CyberDefenseShield";
+export * from "./DatabaseIntegrityShield";
+export * from "./FortressRuntime";
+export * from "./FortressShield";
+export * from "./IntegrityVerificationEngine";
+export * from "./IsolationLayer";
+export * from "./ProtectedAccountManager";
+export * from "./RecoveryEngine";
+export * from "./SecurityContext";
+export * from "./SecurityResult";
+export * from "./ThreatDetectionShield";
+export * from "./TrafficProtectionShield";

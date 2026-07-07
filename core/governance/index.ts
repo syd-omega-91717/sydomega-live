c90a5336773472947da6fe2ccb0eef7f0faa4941
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 02:02:20 +0300

    Create index.ts to export governance modules

---
+++ core/governance/index.ts
@@ -0,0 +1,21 @@
+// ============================================================================
+// FILE:
+// /core/governance/index.ts
+// ============================================================================
+
+export * from "./AdministrationEngine";
+export * from "./DiplomacyEngine";
+export * from "./EmpowermentEngine";
+export * from "./GovernanceAudit";
+export * from "./GovernanceContext";
+export * from "./GovernanceEngine";
+export * from "./GovernancePipeline";
+export * from "./GovernancePolicy";
+export * from "./GovernanceResult";
+export * from "./GovernanceStage";
+export * from "./GuidanceEngine";
+export * from "./InformationEngine";
+export * from "./InterconnectionEngine";
+export * from "./KnowledgeEngine";
+export * from "./PowerEngine";
+export * from "./ValidationEngine";

52662a43b25fa6ce4205d9d7cab6f5b9f18b1788
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 11:30:41 +0300

    Create InstructorEngine class for course assignment

---
+++ core/academy/InstructorEngine.ts
@@ -0,0 +1,26 @@
+// ============================================================================
+// FILE:
+// /core/academy/InstructorEngine.ts
+// ============================================================================
+
+export class InstructorEngine{
+
+    assign(
+
+        instructorId:string,
+
+        courseId:string
+
+    ){
+
+        return{
+
+            instructorId,
+
+            courseId
+
+        };
+
+    }
+
+}

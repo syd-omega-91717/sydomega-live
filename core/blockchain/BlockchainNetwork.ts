2968d53c7e0c781fc8fafe03b7a45f0887efa911
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 12:43:34 +0300

    Add BlockchainNetwork enum for various networks

---
+++ core/blockchain/BlockchainNetwork.ts
@@ -0,0 +1,25 @@
+// ============================================================================
+// FOUNDATION KERNEL FK-012
+// FILE:
+// /core/blockchain/BlockchainNetwork.ts
+// ============================================================================
+
+export enum BlockchainNetwork{
+
+    ETHEREUM="ethereum",
+
+    BNB="bnb",
+
+    POLYGON="polygon",
+
+    ARBITRUM="arbitrum",
+
+    OPTIMISM="optimism",
+
+    AVALANCHE="avalanche",
+
+    SOLANA="solana",
+
+    TRON="tron"
+
+}

6c165fc148326bbfa74bfaa9add1feb53069cdaf
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 20:20:14 +0300

    Add TokenizedAsset interface definition

---
+++ enterprise/blockchain/TokenizedAsset.ts
@@ -0,0 +1,20 @@
+// ============================================================================
+// FILE:
+// /enterprise/blockchain/TokenizedAsset.ts
+// ============================================================================
+
+export interface TokenizedAsset{
+
+    id:string;
+
+    symbol:string;
+
+    name:string;
+
+    chain:string;
+
+    totalSupply:number;
+
+    decimals:number;
+
+}

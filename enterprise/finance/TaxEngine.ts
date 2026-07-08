d73b2cf0b5498c99c12c4736dd388fdd14b5b559
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 21:15:41 +0300

    Create TaxEngine class for tax estimation

---
+++ enterprise/finance/TaxEngine.ts
@@ -0,0 +1,20 @@
+// ============================================================================
+// FILE:
+// /enterprise/finance/TaxEngine.ts
+// ============================================================================
+
+export class TaxEngine{
+
+    estimate(
+
+        amount:number,
+
+        rate:number
+
+    ){
+
+        return amount*rate;
+
+    }
+
+}

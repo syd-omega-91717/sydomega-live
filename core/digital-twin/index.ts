f01e61499134f3e921552ed3c1406eab407502f5
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 12:26:01 +0300

    Create index.ts to export digital twin modules

---
+++ core/digital-twin/index.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /core/digital-twin/index.ts
+// ============================================================================
+
+export * from "./DigitalTwinRuntime";
+export * from "./EventEngine";
+export * from "./GISEngine";
+export * from "./PredictiveEngine";
+export * from "./ScenarioEngine";
+export * from "./SimulationEngine";
+export * from "./SynchronizationEngine";
+export * from "./TelemetryEngine";
+export * from "./TwinAsset";
+export * from "./TwinEntity";
+export * from "./TwinRepository";
+export * from "./TwinSensor";
+export * from "./VisualizationAdapter";

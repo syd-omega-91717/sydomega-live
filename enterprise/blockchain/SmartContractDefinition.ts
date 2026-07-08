1d424a7b3b68d60da46b6ed7f591a287b5b24062
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 20:19:19 +0300

    Create SmartContractDefinition interface

---
+++ enterprise/blockchain/SmartContractDefinition.ts
@@ -0,0 +1,20 @@
+// ============================================================================
+// FILE:
+// /enterprise/blockchain/SmartContractDefinition.ts
+// ============================================================================
+
+export interface SmartContractDefinition{
+
+    id:string;
+
+    name:string;
+
+    chain:string;
+
+    address:string;
+
+    abi:string;
+
+    version:string;
+
+}

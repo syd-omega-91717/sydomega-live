3534b741a9c154a04a57f15dd8eb2436eee946bf
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 20:39:41 +0300

    Create Satellite interface for satellite data

---
+++ enterprise/space/Satellite.ts
@@ -0,0 +1,21 @@
+// ============================================================================
+// ENTERPRISE CORE EC-015
+// FILE:
+// /enterprise/space/Satellite.ts
+// ============================================================================
+
+export interface Satellite{
+
+    id:string;
+
+    name:string;
+
+    constellation:string;
+
+    orbit:string;
+
+    status:string;
+
+    lastContact:number;
+
+}

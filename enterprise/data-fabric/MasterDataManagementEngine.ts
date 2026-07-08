f967a4ade7ad34651364578d80ece9dffd7ae080
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 16:24:19 +0300

    Add MasterDataManagementEngine class with synchronize method

---
+++ enterprise/data-fabric/MasterDataManagementEngine.ts
@@ -0,0 +1,24 @@
+// ============================================================================
+// FILE:
+// /enterprise/data-fabric/MasterDataManagementEngine.ts
+// ============================================================================
+
+export class MasterDataManagementEngine{
+
+    synchronize(
+
+        entity:string
+
+    ){
+
+        return{
+
+            entity,
+
+            synchronized:true
+
+        };
+
+    }
+
+}

2c9f135465afeb5db11755cb856f2e0d9c2340a2
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 16:19:10 +0300

    Create DataAsset.ts

---
+++ enterprise/data-fabric/DataAsset.ts
@@ -0,0 +1,23 @@
+// ============================================================================
+// ENTERPRISE CORE EC-011
+// FILE:
+// /enterprise/data-fabric/DataAsset.ts
+// ============================================================================
+
+export interface DataAsset{
+
+    id:string;
+
+    name:string;
+
+    domain:string;
+
+    owner:string;
+
+    classification:string;
+
+    format:string;
+
+    createdAt:number;
+
+}

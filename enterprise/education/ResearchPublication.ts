8a86461380aec30434ec52fc795dffa3fd17fc6c
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 22:13:04 +0300

    Create ResearchPublication interface

---
+++ enterprise/education/ResearchPublication.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/education/ResearchPublication.ts
+// ============================================================================
+
+export interface ResearchPublication{
+
+    id:string;
+
+    title:string;
+
+    authors:string[];
+
+    journal:string;
+
+    publishedAt:number;
+
+}

60df56c8d0369e56fdbe636018e61991aa8e62db
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 12:52:59 +0300

    Create index.ts to export blockchain modules

---
+++ core/blockchain/index.ts
@@ -0,0 +1,20 @@
+// ============================================================================
+// FILE:
+// /core/blockchain/index.ts
+// ============================================================================
+
+export * from "./BlockchainAuditEngine";
+export * from "./BlockchainNetwork";
+export * from "./BlockchainRuntime";
+export * from "./BridgeEngine";
+export * from "./ContractRegistry";
+export * from "./CustodyEngine";
+export * from "./GovernanceVotingEngine";
+export * from "./NFTEngine";
+export * from "./OmegaTokenEngine";
+export * from "./StakingEngine";
+export * from "./Transaction";
+export * from "./TransactionEngine";
+export * from "./Wallet";
+export * from "./WalletEngine";
+export * from "./Web3Gateway";

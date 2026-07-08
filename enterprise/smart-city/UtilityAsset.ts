c46e8a07fcc8ffcfa85162fcdc6d36841d1cc1f1
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 22:31:57 +0300

    Create UtilityAsset interface for utility assets

---
+++ enterprise/smart-city/UtilityAsset.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/smart-city/UtilityAsset.ts
+// ============================================================================
+
+export interface UtilityAsset{
+
+    id:string;
+
+    type:string;
+
+    operator:string;
+
+    status:string;
+
+    location:string;
+
+}

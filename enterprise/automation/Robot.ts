bc287c5db7f6c95b7b3e1b9e080224312d78d2ca
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 17:58:01 +0300

    Create Robot.ts

---
+++ enterprise/automation/Robot.ts
@@ -0,0 +1,21 @@
+// ============================================================================
+// ENTERPRISE CORE EC-012
+// FILE:
+// /enterprise/automation/Robot.ts
+// ============================================================================
+
+export interface Robot{
+
+    id:string;
+
+    code:string;
+
+    name:string;
+
+    type:string;
+
+    status:string;
+
+    enabled:boolean;
+
+}

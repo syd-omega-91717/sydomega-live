bb4f6a84fa29b08ab338713f78d505d26afd5c1d
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 12:19:19 +0300

    Create TwinEntity.ts

---
+++ core/digital-twin/TwinEntity.ts
@@ -0,0 +1,25 @@
+// ============================================================================
+// FOUNDATION KERNEL FK-010
+// FILE:
+// /core/digital-twin/TwinEntity.ts
+// ============================================================================
+
+export interface TwinEntity{
+
+    id:string;
+
+    code:string;
+
+    name:string;
+
+    type:string;
+
+    status:string;
+
+    location:string;
+
+    createdAt:number;
+
+    updatedAt:number;
+
+}

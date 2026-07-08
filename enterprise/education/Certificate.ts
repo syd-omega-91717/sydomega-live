824ccb5bc0bd4d8af47aa22c329e6d6821fc1e53
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 22:12:37 +0300

    Create Certificate interface for education module

---
+++ enterprise/education/Certificate.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/education/Certificate.ts
+// ============================================================================
+
+export interface Certificate{
+
+    id:string;
+
+    learnerId:string;
+
+    courseId:string;
+
+    issuedAt:number;
+
+    verificationCode:string;
+
+}

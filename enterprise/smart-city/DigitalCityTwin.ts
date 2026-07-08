e5e89ddef991e5f01fc9c22bb8a303e006c3dde6
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 22:31:01 +0300

    Add DigitalCityTwin interface definition

---
+++ enterprise/smart-city/DigitalCityTwin.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE:
+// /enterprise/smart-city/DigitalCityTwin.ts
+// ============================================================================
+
+export interface DigitalCityTwin{
+
+    id:string;
+
+    cityId:string;
+
+    version:string;
+
+    synchronized:boolean;
+
+}

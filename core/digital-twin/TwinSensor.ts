b204b0095b67a71e1201cf53c36399b82b27dad2
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 12:20:16 +0300

    Create TwinSensor.ts

---
+++ core/digital-twin/TwinSensor.ts
@@ -0,0 +1,20 @@
+// ============================================================================
+// FILE:
+// /core/digital-twin/TwinSensor.ts
+// ============================================================================
+
+export interface TwinSensor{
+
+    id:string;
+
+    assetId:string;
+
+    type:string;
+
+    unit:string;
+
+    value:number;
+
+    timestamp:number;
+
+}

6204d9053bc4fb78d3238610c5a6526a3b2f4e4e
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 16:25:10 +0300

    Create index.ts to export data fabric modules

---
+++ enterprise/data-fabric/index.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/data-fabric/index.ts
+// ============================================================================
+
+export * from "./DataAsset";
+export * from "./DataFabricEngine";
+export * from "./DataFabricOrchestrator";
+export * from "./DataGovernanceEngine";
+export * from "./DataLineage";
+export * from "./DataQualityEngine";
+export * from "./LakehouseEngine";
+export * from "./LineageEngine";
+export * from "./MasterDataManagementEngine";
+export * from "./MetadataCatalog";
+export * from "./MetadataRecord";
+export * from "./SchemaRegistry";
+export * from "./StreamingIngestionEngine";

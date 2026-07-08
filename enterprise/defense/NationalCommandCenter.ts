42d252984a5bcd75cd201cc895dcdf1d0e072610
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 01:29:56 +0300

    Create NationalCommandCenter.ts

---
+++ enterprise/defense/NationalCommandCenter.ts
@@ -0,0 +1,19 @@
+// ============================================================================
+// ENTERPRISE CORE EC-023
+// FILE:
+// /enterprise/defense/NationalCommandCenter.ts
+// ============================================================================
+
+export interface NationalCommandCenter{
+
+    id:string;
+
+    name:string;
+
+    jurisdiction:string;
+
+    operational:boolean;
+
+    commander:string;
+
+}

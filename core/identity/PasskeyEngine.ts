223f73e5de90b159edd42a0550da93f950098fbc
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 12:35:40 +0300

    Create PasskeyEngine class for user registration

---
+++ core/identity/PasskeyEngine.ts
@@ -0,0 +1,24 @@
+// ============================================================================
+// FILE:
+// /core/identity/PasskeyEngine.ts
+// ============================================================================
+
+export class PasskeyEngine{
+
+    register(
+
+        userId:string
+
+    ){
+
+        return{
+
+            userId,
+
+            registered:true
+
+        };
+
+    }
+
+}

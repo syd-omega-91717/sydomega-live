7c3f1fadfdf823a3ad2617e125834660e16ed2ca
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 09:40:04 +0300

    Add AlchemyStage enum for alchemical processes

---
+++ core/alchemy/AlchemyStage.ts
@@ -0,0 +1,17 @@
+// ============================================================================
+// FOUNDATION KERNEL FK-006
+// FILE:
+// /core/alchemy/AlchemyStage.ts
+// ============================================================================
+
+export enum AlchemyStage{
+
+    NIGREDO=1,
+
+    ALBEDO,
+
+    CITRINITAS,
+
+    RUBEDO
+
+}

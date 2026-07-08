78d7f1a68ebcb0910801fce169a6bc84943e29a9
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 15:15:07 +0300

    Create index.ts

---
+++ enterprise/assets/index.ts
@@ -0,0 +1,21 @@
+// ============================================================================
+// FILE:
+// /enterprise/assets/index.ts
+// ============================================================================
+
+export * from "./Asset";
+export * from "./AssetRegistry";
+export * from "./Calibration";
+export * from "./CalibrationEngine";
+export * from "./EquipmentLifecycleEngine";
+export * from "./Facility";
+export * from "./FacilityManagementEngine";
+export * from "./Inspection";
+export * from "./InspectionEngine";
+export * from "./InventoryEngine";
+export * from "./IoTIntegrationEngine";
+export * from "./MaintenanceRuntime";
+export * from "./PreventiveMaintenanceEngine";
+export * from "./SparePart";
+export * from "./WorkOrder";
+export * from "./WorkOrderEngine";

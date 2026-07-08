49685a9084ffe9bb5f3c92884bee4687d73419d4
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 18:03:00 +0300

    Create AutomationMonitoringEngine class for monitoring

---
+++ enterprise/automation/AutomationMonitoringEngine.ts
@@ -0,0 +1,22 @@
+// ============================================================================
+// FILE:
+// /enterprise/automation/AutomationMonitoringEngine.ts
+// ============================================================================
+
+export class AutomationMonitoringEngine{
+
+    monitor(){
+
+        return{
+
+            running:0,
+
+            queued:0,
+
+            failed:0
+
+        };
+
+    }
+
+}

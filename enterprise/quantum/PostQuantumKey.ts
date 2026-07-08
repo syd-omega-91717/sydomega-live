384013bce94ba9e9fd41d8d65ba1bafd9198cc64
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 20:29:02 +0300

    Add PostQuantumKey interface definition

---
+++ enterprise/quantum/PostQuantumKey.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/quantum/PostQuantumKey.ts
+// ============================================================================
+
+export interface PostQuantumKey{
+
+    id:string;
+
+    algorithm:string;
+
+    publicKey:string;
+
+    privateKey:string;
+
+    createdAt:number;
+
+}

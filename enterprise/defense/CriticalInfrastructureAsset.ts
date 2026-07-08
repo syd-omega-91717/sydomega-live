af370e93220647d8af973e60b75f53f52c51e82a
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 01:31:19 +0300

    Create CriticalInfrastructureAsset interface

---
+++ enterprise/defense/CriticalInfrastructureAsset.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/defense/CriticalInfrastructureAsset.ts
+// ============================================================================
+
+export interface CriticalInfrastructureAsset{
+
+    id:string;
+
+    name:string;
+
+    category:string;
+
+    protectionLevel:string;
+
+    status:string;
+
+}

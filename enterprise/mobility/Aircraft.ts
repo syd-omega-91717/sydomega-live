eeb82708d4ce9a2d7b7fe2bfdbacf5936845a228
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 01:50:26 +0300

    Create Aircraft interface for mobility module

---
+++ enterprise/mobility/Aircraft.ts
@@ -0,0 +1,19 @@
+// ============================================================================
+// ENTERPRISE CORE EC-024
+// FILE:
+// /enterprise/mobility/Aircraft.ts
+// ============================================================================
+
+export interface Aircraft{
+
+    id:string;
+
+    registration:string;
+
+    type:string;
+
+    operator:string;
+
+    status:string;
+
+}

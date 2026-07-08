8e65dbd871d4805600a318ee1062fa67ea240af8
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 22:28:38 +0300

    Create index.ts for manufacturing module exports

---
+++ enterprise/manufacturing/index.ts
@@ -0,0 +1,19 @@
+// ============================================================================
+// FILE:
+// /enterprise/manufacturing/index.ts
+// ============================================================================
+
+export * from "./DigitalFactoryTwinEngine";
+export * from "./IndustrialAIEngine";
+export * from "./IndustrialDevice";
+export * from "./IndustrialIoTEngine";
+export * from "./IndustrialOrchestrator";
+export * from "./ManufacturingExecutionEngine";
+export * from "./OPCUAGateway";
+export * from "./PLCController";
+export * from "./PredictiveMaintenanceEngine";
+export * from "./ProductionOrder";
+export * from "./ProductionPlanningEngine";
+export * from "./QualityManagementEngine";
+export * from "./SCADAEngine";
+export * from "./SupplyChainSynchronizationEngine";

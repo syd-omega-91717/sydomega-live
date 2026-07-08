b1940d5eaa0747ae6190ecd9d1e35c769e27a512
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 11:24:28 +0300

    Create AcademyCourse.ts

---
+++ core/academy/AcademyCourse.ts
@@ -0,0 +1,27 @@
+// ============================================================================
+// FOUNDATION KERNEL FK-008
+// FILE:
+// /core/academy/AcademyCourse.ts
+// ============================================================================
+
+export interface AcademyCourse{
+
+    id:string;
+
+    code:string;
+
+    title:string;
+
+    description:string;
+
+    category:string;
+
+    level:number;
+
+    duration:number;
+
+    price:number;
+
+    published:boolean;
+
+}

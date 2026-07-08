7d575037f37cafac1a2ee39003625e7493879d13
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 15:04:58 +0300

    Create Facility.ts

---
+++ enterprise/assets/Facility.ts
@@ -0,0 +1,22 @@
+// ============================================================================
+// FILE:
+// /enterprise/assets/Facility.ts
+// ============================================================================
+
+export interface Facility{
+
+    id:string;
+
+    code:string;
+
+    name:string;
+
+    country:string;
+
+    city:string;
+
+    address:string;
+
+    active:boolean;
+
+}

6ea8c817193c6060eef333f2a5cf5a2af7843003
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 01:56:15 +0300

    Create index.ts to export mobility modules

---
+++ enterprise/mobility/index.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/mobility/index.ts
+// ============================================================================
+
+export * from "./Aircraft";
+export * from "./AirportOperationsEngine";
+export * from "./AirTrafficManagementEngine";
+export * from "./AerospaceOperationsOrchestrator";
+export * from "./AutonomousMobilityEngine";
+export * from "./FleetManagementEngine";
+export * from "./LogisticsMobilityEngine";
+export * from "./MaritimeOperationsEngine";
+export * from "./NavigationEngine";
+export * from "./PortManagementEngine";
+export * from "./UAV";
+export * from "./UAVOperationsEngine";
+export * from "./Vessel";

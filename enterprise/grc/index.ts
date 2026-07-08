5b16517adab91f491171fdfaab650141c551a0da
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 21:06:20 +0300

    Create index.ts to export GRC modules

---
+++ enterprise/grc/index.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/grc/index.ts
+// ============================================================================
+
+export * from "./AuditEngine";
+export * from "./AuditRecord";
+export * from "./ComplianceEngine";
+export * from "./ComplianceFramework";
+export * from "./EvidenceRepository";
+export * from "./ExceptionTrackingEngine";
+export * from "./GovernanceDashboardEngine";
+export * from "./GRCOrchestrator";
+export * from "./InternalControl";
+export * from "./Policy";
+export * from "./RegulatoryReportingEngine";
+export * from "./RiskEngine";
+export * from "./RiskRecord";

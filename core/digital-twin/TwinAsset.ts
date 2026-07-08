4176011791851059c5a99a619356694b17222e9a
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 12:19:47 +0300

    Create TwinAsset interface for digital twin model

---
+++ core/digital-twin/TwinAsset.ts
@@ -0,0 +1,20 @@
+// ============================================================================
+// FILE:
+// /core/digital-twin/TwinAsset.ts
+// ============================================================================
+
+export interface TwinAsset{
+
+    id:string;
+
+    entityId:string;
+
+    assetType:string;
+
+    manufacturer:string;
+
+    serialNumber:string;
+
+    health:number;
+
+}

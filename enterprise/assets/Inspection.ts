d3315724249b1634a96e1b0f15e55d0e1d88f316
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 15:06:24 +0300

    Create Inspection.ts

---
+++ enterprise/assets/Inspection.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/assets/Inspection.ts
+// ============================================================================
+
+export interface Inspection{
+
+    id:string;
+
+    assetId:string;
+
+    inspector:string;
+
+    status:string;
+
+    inspectionDate:number;
+
+}

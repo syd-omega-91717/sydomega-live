56a2f89200227d20ddf07120dbf0fbc6c2ea029f
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 21:11:34 +0300

    Create DigitalAssetPosition interface

---
+++ enterprise/finance/DigitalAssetPosition.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/finance/DigitalAssetPosition.ts
+// ============================================================================
+
+export interface DigitalAssetPosition{
+
+    id:string;
+
+    symbol:string;
+
+    quantity:number;
+
+    valuation:number;
+
+    chain:string;
+
+}

85a4e2564dc388b5d92b6ae8de888b84d0c5c309
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 16:00:34 +0300

    Create Ontology.ts

---
+++ enterprise/knowledge/Ontology.ts
@@ -0,0 +1,19 @@
+// ============================================================================
+// ENTERPRISE CORE EC-009
+// FILE:
+// /enterprise/knowledge/Ontology.ts
+// ============================================================================
+
+export interface Ontology{
+
+    id:string;
+
+    name:string;
+
+    version:string;
+
+    description:string;
+
+    createdAt:number;
+
+}

18747991b51827f45d8287107bd8dda103f1f0ad
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 18:03:52 +0300

    Create index.ts to export automation modules

---
+++ enterprise/automation/index.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/automation/index.ts
+// ============================================================================
+
+export * from "./AutomationJob";
+export * from "./AutomationMonitoringEngine";
+export * from "./AutomationRuntime";
+export * from "./AutomationScheduler";
+export * from "./AutomationTask";
+export * from "./DigitalWorkerEngine";
+export * from "./DocumentProcessingEngine";
+export * from "./OCREngine";
+export * from "./Robot";
+export * from "./RobotOrchestrator";
+export * from "./RPAEngine";
+export * from "./UnattendedExecutionEngine";
+export * from "./WorkflowBotEngine";

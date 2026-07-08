a3f62ef6c92c55e489545d6b7ba392e28a01f761
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 22:09:07 +0300

    Create index.ts to export healthcare modules

---
+++ enterprise/healthcare/index.ts
@@ -0,0 +1,19 @@
+// ============================================================================
+// FILE:
+// /enterprise/healthcare/index.ts
+// ============================================================================
+
+export * from "./BiomedicalAnalyticsEngine";
+export * from "./ClinicalDecisionSupportEngine";
+export * from "./DICOMStudy";
+export * from "./ElectronicHealthRecord";
+export * from "./FHIRGateway";
+export * from "./FHIRResource";
+export * from "./GenomicsEngine";
+export * from "./HealthcareOrchestrator";
+export * from "./LaboratoryEngine";
+export * from "./LaboratoryResult";
+export * from "./PACSEngine";
+export * from "./Patient";
+export * from "./PatientDigitalTwinEngine";
+export * from "./ProteomicsEngine";

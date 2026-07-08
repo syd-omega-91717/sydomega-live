12cfb8bb80e01d2753703106b448d17170d425c7
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 22:30:32 +0300

    Create City interface for smart city module

---
+++ enterprise/smart-city/City.ts
@@ -0,0 +1,19 @@
+// ============================================================================
+// ENTERPRISE CORE EC-022
+// FILE:
+// /enterprise/smart-city/City.ts
+// ============================================================================
+
+export interface City{
+
+    id:string;
+
+    name:string;
+
+    country:string;
+
+    population:number;
+
+    timezone:string;
+
+}

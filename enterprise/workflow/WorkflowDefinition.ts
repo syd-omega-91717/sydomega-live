4353140a87fd79b87b2a2737cdb744020940e35b
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 14:38:47 +0300

    Add WorkflowDefinition interface

---
+++ enterprise/workflow/WorkflowDefinition.ts
@@ -0,0 +1,21 @@
+// ============================================================================
+// ENTERPRISE CORE EC-003
+// FILE:
+// /enterprise/workflow/WorkflowDefinition.ts
+// ============================================================================
+
+export interface WorkflowDefinition{
+
+    id:string;
+
+    code:string;
+
+    name:string;
+
+    version:number;
+
+    enabled:boolean;
+
+    createdAt:number;
+
+}

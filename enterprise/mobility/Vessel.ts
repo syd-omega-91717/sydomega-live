719567566b57151a006d386391276a145bdd0c4b
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 01:50:55 +0300

    Create Vessel interface for mobility module

---
+++ enterprise/mobility/Vessel.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/mobility/Vessel.ts
+// ============================================================================
+
+export interface Vessel{
+
+    id:string;
+
+    imo:string;
+
+    name:string;
+
+    flag:string;
+
+    status:string;
+
+}

0e183c716708a4a44947b3f3b2e1e7d767ac3c46
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 01:51:20 +0300

    Create UAV interface for drone properties

---
+++ enterprise/mobility/UAV.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/mobility/UAV.ts
+// ============================================================================
+
+export interface UAV{
+
+    id:string;
+
+    serialNumber:string;
+
+    model:string;
+
+    autonomyLevel:number;
+
+    operational:boolean;
+
+}

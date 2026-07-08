fccce7946921b22da260bf6ca846e98a97de5007
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 11:25:37 +0300

    Create AcademyEnrollment interface for course data

---
+++ core/academy/AcademyEnrollment.ts
@@ -0,0 +1,20 @@
+// ============================================================================
+// FILE:
+// /core/academy/AcademyEnrollment.ts
+// ============================================================================
+
+export interface AcademyEnrollment{
+
+    id:string;
+
+    userId:string;
+
+    courseId:string;
+
+    enrolledAt:number;
+
+    completed:boolean;
+
+    completion:number;
+
+}

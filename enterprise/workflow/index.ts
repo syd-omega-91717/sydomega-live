6899d40209f99b02dafe9678d06c14d6eb43a862
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 14:47:58 +0300

    Create index.ts to export workflow modules

---
+++ enterprise/workflow/index.ts
@@ -0,0 +1,19 @@
+// ============================================================================
+// FILE:
+// /enterprise/workflow/index.ts
+// ============================================================================
+
+export * from "./ApprovalEngine";
+export * from "./BPMNWorkflowEngine";
+export * from "./BusinessRuleEngine";
+export * from "./SchedulerEngine";
+export * from "./SLAMonitor";
+export * from "./TaskEngine";
+export * from "./WorkflowAnalyticsEngine";
+export * from "./WorkflowDefinition";
+export * from "./WorkflowExecutionEngine";
+export * from "./WorkflowInstance";
+export * from "./WorkflowNotificationEngine";
+export * from "./WorkflowRepository";
+export * from "./WorkflowRuntime";
+export * from "./WorkflowTask";

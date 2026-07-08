256c6888d6caa7283b441c2f96d606cc4461f0a5
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 17:58:29 +0300

    Create AutomationJob interface for automation tasks

---
+++ enterprise/automation/AutomationJob.ts
@@ -0,0 +1,20 @@
+// ============================================================================
+// FILE:
+// /enterprise/automation/AutomationJob.ts
+// ============================================================================
+
+export interface AutomationJob{
+
+    id:string;
+
+    name:string;
+
+    robotId:string;
+
+    priority:string;
+
+    schedule:string;
+
+    status:string;
+
+}

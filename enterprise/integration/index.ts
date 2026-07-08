2cdd8ce25086b95ca0ca5f8c6e5a5000b88ff781
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 13:13:27 +0300

    Create index.ts

---
+++ enterprise/integration/index.ts
@@ -0,0 +1,15 @@
+// ============================================================================
+// FILE:
+// /enterprise/integration/index.ts
+// ============================================================================
+
+export * from "./APIRegistry";
+export * from "./DataFederationEngine";
+export * from "./EnterpriseServiceBus";
+export * from "./ETLPipeline";
+export * from "./EventStreamingEngine";
+export * from "./GraphQLGateway";
+export * from "./IntegrationOrchestrator";
+export * from "./MessageQueue";
+export * from "./RestGateway";
+export * from "./WebhookEngine";

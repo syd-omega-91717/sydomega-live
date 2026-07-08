f01dd725d22f8454d7a2a5267b284a92621857c5
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 20:28:32 +0300

    Create QuantumJob interface for quantum jobs

---
+++ enterprise/quantum/QuantumJob.ts
@@ -0,0 +1,19 @@
+// ============================================================================
+// ENTERPRISE CORE EC-014
+// FILE:
+// /enterprise/quantum/QuantumJob.ts
+// ============================================================================
+
+export interface QuantumJob{
+
+    id:string;
+
+    algorithm:string;
+
+    backend:string;
+
+    status:string;
+
+    createdAt:number;
+
+}

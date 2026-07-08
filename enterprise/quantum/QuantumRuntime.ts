859b212cbcc6a634d5d64eca5566f6577f20ab86
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 20:29:57 +0300

    Add QuantumRuntime interface definition

---
+++ enterprise/quantum/QuantumRuntime.ts
@@ -0,0 +1,14 @@
+// ============================================================================
+// FILE:
+// /enterprise/quantum/QuantumRuntime.ts
+// ============================================================================
+
+export interface QuantumRuntime{
+
+    backend:string;
+
+    provider:string;
+
+    simulator:boolean;
+
+}

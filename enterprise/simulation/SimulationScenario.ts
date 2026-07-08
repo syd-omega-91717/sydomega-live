229369300ced6a780c27d1b54f1f456e8921101d
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 20:48:44 +0300

    Create SimulationScenario interface for simulation

---
+++ enterprise/simulation/SimulationScenario.ts
@@ -0,0 +1,19 @@
+// ============================================================================
+// ENTERPRISE CORE EC-016
+// FILE:
+// /enterprise/simulation/SimulationScenario.ts
+// ============================================================================
+
+export interface SimulationScenario{
+
+    id:string;
+
+    name:string;
+
+    description:string;
+
+    duration:number;
+
+    status:string;
+
+}

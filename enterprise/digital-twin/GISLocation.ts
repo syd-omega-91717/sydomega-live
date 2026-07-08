53d6926bc983f97bc8d01463a692699ca926dcf0
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 15:19:07 +0300

    Create GISLocation interface for digital twin

---
+++ enterprise/digital-twin/GISLocation.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/digital-twin/GISLocation.ts
+// ============================================================================
+
+export interface GISLocation{
+
+    id:string;
+
+    latitude:number;
+
+    longitude:number;
+
+    altitude:number;
+
+    geofence:string;
+
+}

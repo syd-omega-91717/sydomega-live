6fc12af105f1a3260b88bd14681b901ea7acb689
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 21:01:39 +0300

    Add InternalControl interface definition

---
+++ enterprise/grc/InternalControl.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/grc/InternalControl.ts
+// ============================================================================
+
+export interface InternalControl{
+
+    id:string;
+
+    name:string;
+
+    frameworkId:string;
+
+    owner:string;
+
+    implemented:boolean;
+
+}

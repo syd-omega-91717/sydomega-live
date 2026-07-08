0c2f46d8db3da516d4b860b89b8b9b8f4a748bbc
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 20:59:22 +0300

    Create Policy interface for enterprise GRC module

---
+++ enterprise/grc/Policy.ts
@@ -0,0 +1,21 @@
+// ============================================================================
+// ENTERPRISE CORE EC-017
+// FILE:
+// /enterprise/grc/Policy.ts
+// ============================================================================
+
+export interface Policy{
+
+    id:string;
+
+    name:string;
+
+    version:string;
+
+    owner:string;
+
+    status:string;
+
+    effectiveDate:number;
+
+}

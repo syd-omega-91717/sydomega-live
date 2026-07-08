8abee856bb729fcc348fd8199186f71ab8a83412
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 12:21:25 +0300

    Create GISEngine class for location handling

---
+++ core/digital-twin/GISEngine.ts
@@ -0,0 +1,26 @@
+// ============================================================================
+// FILE:
+// /core/digital-twin/GISEngine.ts
+// ============================================================================
+
+export class GISEngine{
+
+    locate(
+
+        latitude:number,
+
+        longitude:number
+
+    ){
+
+        return{
+
+            latitude,
+
+            longitude
+
+        };
+
+    }
+
+}

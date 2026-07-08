a71a39a236a5b7ae991a6a2bf31b443ba85c90ae
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 01:30:48 +0300

    Create BorderCheckpoint interface for border data

---
+++ enterprise/defense/BorderCheckpoint.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE:
+// /enterprise/defense/BorderCheckpoint.ts
+// ============================================================================
+
+export interface BorderCheckpoint{
+
+    id:string;
+
+    name:string;
+
+    country:string;
+
+    operational:boolean;
+
+}

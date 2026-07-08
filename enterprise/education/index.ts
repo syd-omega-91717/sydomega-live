d14f79f48b2c7f1887c0f381cec513e8b744fcc8
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 22:18:03 +0300

    Create index.ts to export education modules

---
+++ enterprise/education/index.ts
@@ -0,0 +1,19 @@
+// ============================================================================
+// FILE:
+// /enterprise/education/index.ts
+// ============================================================================
+
+export * from "./AcademicAnalyticsEngine";
+export * from "./AdaptiveLearningEngine";
+export * from "./Assessment";
+export * from "./AssessmentEngine";
+export * from "./Certificate";
+export * from "./CertificationEngine";
+export * from "./Course";
+export * from "./DigitalCredentialEngine";
+export * from "./KnowledgeDiscoveryEngine";
+export * from "./LearningManagementEngine";
+export * from "./Lesson";
+export * from "./ResearchPublication";
+export * from "./ResearchRepository";
+export * from "./SovereignAcademyOrchestrator";

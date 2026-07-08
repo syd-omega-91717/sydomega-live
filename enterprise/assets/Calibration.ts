62754fdec1b9da51711ffa68b7859b7d2eba4f97
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 15:07:04 +0300

    Create Calibration.ts

---
+++ enterprise/assets/Calibration.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/assets/Calibration.ts
+// ============================================================================
+
+export interface Calibration{
+
+    id:string;
+
+    assetId:string;
+
+    technician:string;
+
+    calibrationDate:number;
+
+    nextCalibration:number;
+
+}

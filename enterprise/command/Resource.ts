25f4a8a1fc7d001cda2d595604a4c7089dac9301
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 15:49:43 +0300

    Add Resource interface definition

---
+++ enterprise/command/Resource.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/command/Resource.ts
+// ============================================================================
+
+export interface Resource{
+
+    id:string;
+
+    type:string;
+
+    name:string;
+
+    location:string;
+
+    available:boolean;
+
+}

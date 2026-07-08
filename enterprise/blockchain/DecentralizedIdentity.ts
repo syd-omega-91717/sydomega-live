be8aa3c963af4dab48806af329d69a111d92206d
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 20:18:13 +0300

    Add DecentralizedIdentity interface definition

---
+++ enterprise/blockchain/DecentralizedIdentity.ts
@@ -0,0 +1,21 @@
+// ============================================================================
+// ENTERPRISE CORE EC-013
+// FILE:
+// /enterprise/blockchain/DecentralizedIdentity.ts
+// ============================================================================
+
+export interface DecentralizedIdentity{
+
+    id:string;
+
+    did:string;
+
+    owner:string;
+
+    publicKey:string;
+
+    status:string;
+
+    createdAt:number;
+
+}

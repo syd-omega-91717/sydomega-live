b66e451f9504b71a1586f4b50a0ddca7d426e224
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-08 15:05:57 +0300

    Create SparePart interface for spare parts management

---
+++ enterprise/assets/SparePart.ts
@@ -0,0 +1,20 @@
+// ============================================================================
+// FILE:
+// /enterprise/assets/SparePart.ts
+// ============================================================================
+
+export interface SparePart{
+
+    id:string;
+
+    sku:string;
+
+    name:string;
+
+    quantity:number;
+
+    minimumQuantity:number;
+
+    warehouse:string;
+
+}

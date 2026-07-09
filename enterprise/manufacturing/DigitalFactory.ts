bdb8aecb821cfac2e44f3a49d341440443a124bc
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 11:08:13 +0300

    Create DigitalFactory interface for manufacturing

---
+++ enterprise/manufacturing/DigitalFactory.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE:
+// /enterprise/manufacturing/DigitalFactory.ts
+// ============================================================================
+
+export interface DigitalFactory{
+
+    id:string;
+
+    name:string;
+
+    site:string;
+
+    twinId:string;
+
+}

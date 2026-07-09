e7ff77170fdb1937f980650d8bb3ec37d8b2c145
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 13:01:59 +0300

    Create Student.ts

---
+++ enterprise/education/Student.ts
@@ -0,0 +1,19 @@
+// ============================================================================
+// ENTERPRISE CORE EC-037
+// FILE:
+// /enterprise/education/Student.ts
+// ============================================================================
+
+export interface Student{
+
+    id:string;
+
+    fullName:string;
+
+    email:string;
+
+    enrollmentStatus:string;
+
+    academyId:string;
+
+}

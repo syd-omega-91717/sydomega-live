13a78249b64c4045585bf5a730659d1b4560e202
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 22:22:04 +0300

    Add index.ts to export kernel modules

---
+++ enterprise/kernel/index.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/kernel/index.ts
+// ============================================================================
+
+export * from "./CrossDomainWorkflowCoordinator";
+export * from "./EnterpriseCivilizationKernelOrchestrator";
+export * from "./EnterpriseCommandCenterEngine";
+export * from "./EnterpriseServiceRegistryEngine";
+export * from "./GlobalConfigurationEngine";
+export * from "./GlobalSchedulerEngine";
+export * from "./PlatformCapabilityRegistryEngine";
+export * from "./PlatformService";
+export * from "./SystemCapability";
+export * from "./SystemLifecycleManagerEngine";
+export * from "./SystemPolicy";
+export * from "./UnifiedEventBusEngine";
+export * from "./UnifiedPolicyEngine";

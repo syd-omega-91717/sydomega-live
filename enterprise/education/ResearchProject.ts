1578bd6e24061ce55d9028e4569322acaf703525
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 13:03:24 +0300

    Create ResearchProject.ts

---
+++ enterprise/education/ResearchProject.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/education/ResearchProject.ts
+// ============================================================================
+
+export interface ResearchProject{
+
+    id:string;
+
+    title:string;
+
+    principalInvestigator:string;
+
+    status:string;
+
+    funding:number;
+
+}

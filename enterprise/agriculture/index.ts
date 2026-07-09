d6928842a91f9cac997e309441bffa1adf378512
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 18:50:01 +0300

    Add index.ts for agriculture module exports

---
+++ enterprise/agriculture/index.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/agriculture/index.ts
+// ============================================================================
+
+export * from "./AgriculturalMarketplaceEngine";
+export * from "./AgricultureOperationsOrchestrator";
+export * from "./Crop";
+export * from "./CropAnalyticsEngine";
+export * from "./Farm";
+export * from "./FoodTraceabilityEngine";
+export * from "./GreenhouseAutomationEngine";
+export * from "./IrrigationIntelligenceEngine";
+export * from "./Livestock";
+export * from "./LivestockManagementEngine";
+export * from "./PrecisionAgricultureEngine";
+export * from "./SmartFarmingEngine";
+export * from "./SustainabilityIntelligenceEngine";

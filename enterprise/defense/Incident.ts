c64e71ee4b5b3042db37d04ed016b270ae3319d1
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 10:50:39 +0300

    Create Incident interface for incident management

---
+++ enterprise/defense/Incident.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/defense/Incident.ts
+// ============================================================================
+
+export interface Incident{
+
+    id:string;
+
+    title:string;
+
+    priority:number;
+
+    status:string;
+
+    commander:string;
+
+}

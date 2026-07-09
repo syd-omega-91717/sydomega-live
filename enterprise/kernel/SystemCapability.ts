b88df95415da0014640bb2292ad7e889bd1e0b2e
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 22:09:17 +0300

    Add SystemCapability interface definition

---
+++ enterprise/kernel/SystemCapability.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/kernel/SystemCapability.ts
+// ============================================================================
+
+export interface SystemCapability{
+
+    id:string;
+
+    name:string;
+
+    category:string;
+
+    provider:string;
+
+    available:boolean;
+
+}

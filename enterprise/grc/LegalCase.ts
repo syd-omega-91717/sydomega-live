f33875004c62c01749f934b882620b3612c9d86c
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 19:13:13 +0300

    Create LegalCase interface for legal case management

---
+++ enterprise/grc/LegalCase.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/grc/LegalCase.ts
+// ============================================================================
+
+export interface LegalCase{
+
+    id:string;
+
+    reference:string;
+
+    title:string;
+
+    status:string;
+
+    assignedCounsel:string;
+
+}

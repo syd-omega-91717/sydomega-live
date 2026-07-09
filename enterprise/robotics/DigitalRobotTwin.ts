ae6df2979be1d483b2321a7eeb67a2a0a20224aa
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 11:22:49 +0300

    Add DigitalRobotTwin interface definition

---
+++ enterprise/robotics/DigitalRobotTwin.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE:
+// /enterprise/robotics/DigitalRobotTwin.ts
+// ============================================================================
+
+export interface DigitalRobotTwin{
+
+    id:string;
+
+    robotId:string;
+
+    synchronizationState:string;
+
+    lastUpdated:string;
+
+}

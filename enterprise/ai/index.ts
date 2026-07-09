9d1db0f1a71486ffcb48da47fbb11aea69c7465a
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 07:59:37 +0300

    Add new exports to enterprise AI module

--- enterprise/ai/index.ts
+++ enterprise/ai/index.ts
@@ -3,16 +3,16 @@
 // /enterprise/ai/index.ts
 // ============================================================================
 
-export * from "./AgentCommunicationBus";
-export * from "./AgentRegistry";
-export * from "./AgentTask";
-export * from "./EnterpriseAgent";
-export * from "./EnterpriseCopilot";
-export * from "./MemoryEngine";
-export * from "./ModelRouter";
-export * from "./MultiAgentCoordinator";
-export * from "./PlanningEngine";
-export * from "./SovereignAIOrchestrator";
-export * from "./TaskDecompositionEngine";
-export * from "./ToolDefinition";
-export * from "./ToolRegistry";
+export * from "./Agent";
+export * from "./AgentMemoryEngine";
+export * from "./AgentRuntime";
+export * from "./EnterpriseAIOrchestrator";
+export * from "./KnowledgeGraphEngine";
+export * from "./KnowledgeNode";
+export * from "./LLMGateway";
+export * from "./LLMProvider";
+export * from "./MultiAgentCollaborationEngine";
+export * from "./PromptOrchestrationEngine";
+export * from "./RAGEngine";
+export * from "./VectorDatabaseEngine";
+export * from "./WorkflowAutomationEngine";

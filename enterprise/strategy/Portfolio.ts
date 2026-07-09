1656deb33b5edef7e80026080bccd3ac19634670
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 21:24:06 +0300

    Create Portfolio interface with project attributes

---
+++ enterprise/strategy/Portfolio.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/strategy/Portfolio.ts
+// ============================================================================
+
+export interface Portfolio{
+
+    id:string;
+
+    name:string;
+
+    sponsor:string;
+
+    budget:number;
+
+    active:boolean;
+
+}

49a036fcdc54c745f5452ee97f2676ad3ac8fe2c
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 10:35:14 +0300

    Create index.ts to export government modules

---
+++ enterprise/government/index.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/government/index.ts
+// ============================================================================
+
+export * from "./Citizen";
+export * from "./DigitalIdentityEngine";
+export * from "./EmergencyManagementEngine";
+export * from "./GovernmentOperationsOrchestrator";
+export * from "./IoTDevice";
+export * from "./IoTInfrastructureEngine";
+export * from "./NationalCitizenRegistryEngine";
+export * from "./NationalDigitalTwinEngine";
+export * from "./PublicServicesEngine";
+export * from "./SmartCity";
+export * from "./SmartCityPlatformEngine";
+export * from "./TransportationManagementEngine";
+export * from "./UtilitiesManagementEngine";

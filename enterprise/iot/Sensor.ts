236c7773fac8b6c6b15b527f8948e374655a1396
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 20:43:46 +0300

    Create Sensor interface for IoT module

---
+++ enterprise/iot/Sensor.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/iot/Sensor.ts
+// ============================================================================
+
+export interface Sensor{
+
+    id:string;
+
+    deviceId:string;
+
+    sensorType:string;
+
+    unit:string;
+
+    enabled:boolean;
+
+}

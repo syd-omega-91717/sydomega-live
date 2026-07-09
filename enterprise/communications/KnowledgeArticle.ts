1eb5af5929da8922fdc0b2ae84fcbd712289a4a6
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 19:54:26 +0300

    Create KnowledgeArticle interface for articles

---
+++ enterprise/communications/KnowledgeArticle.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/communications/KnowledgeArticle.ts
+// ============================================================================
+
+export interface KnowledgeArticle{
+
+    id:string;
+
+    title:string;
+
+    authorId:string;
+
+    version:string;
+
+    published:boolean;
+
+}

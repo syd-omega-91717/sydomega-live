d2697ff49619a6978f104a067965f5a4fc773817
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 19:02:38 +0300

    Create RailUnit interface for transport module

---
+++ enterprise/transport/RailUnit.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE:
+// /enterprise/transport/RailUnit.ts
+// ============================================================================
+
+export interface RailUnit{
+
+    id:string;
+
+    number:string;
+
+    line:string;
+
+    operational:boolean;
+
+}

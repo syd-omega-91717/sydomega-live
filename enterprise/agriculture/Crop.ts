b68a4f2e6b2e079c99b376139a9646ec273dc3a6
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 18:42:41 +0300

    Create Crop.ts

---
+++ enterprise/agriculture/Crop.ts
@@ -0,0 +1,19 @@
+// ============================================================================
+// ENTERPRISE CORE EC-041
+// FILE:
+// /enterprise/agriculture/Crop.ts
+// ============================================================================
+
+export interface Crop{
+
+    id:string;
+
+    name:string;
+
+    variety:string;
+
+    plantingDate:string;
+
+    expectedHarvestDate:string;
+
+}

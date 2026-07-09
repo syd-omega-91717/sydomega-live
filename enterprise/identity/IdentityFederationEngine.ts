537385b429f4071a6d7f37dae53e5d770f40d48e
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 19:26:19 +0300

    Create IdentityFederationEngine class for federation

---
+++ enterprise/identity/IdentityFederationEngine.ts
@@ -0,0 +1,24 @@
+// ============================================================================
+// FILE:
+// /enterprise/identity/IdentityFederationEngine.ts
+// ============================================================================
+
+export class IdentityFederationEngine{
+
+    federate(
+
+        provider:string
+
+    ){
+
+        return{
+
+            provider,
+
+            connected:true
+
+        };
+
+    }
+
+}

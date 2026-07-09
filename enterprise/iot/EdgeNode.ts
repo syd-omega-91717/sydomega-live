eabbd4cd124178a977d926d19ad9965c4f5cbae0
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 20:44:35 +0300

    Create EdgeNode interface for IoT module

---
+++ enterprise/iot/EdgeNode.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/iot/EdgeNode.ts
+// ============================================================================
+
+export interface EdgeNode{
+
+    id:string;
+
+    location:string;
+
+    runtime:string;
+
+    status:string;
+
+    connectedDevices:number;
+
+}

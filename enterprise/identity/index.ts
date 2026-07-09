d579de18862f5cb612f5a37167250fb2be8b9ebb
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 19:29:58 +0300

    Add index.ts to export identity modules

---
+++ enterprise/identity/index.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/identity/index.ts
+// ============================================================================
+
+export * from "./BiometricIdentityEngine";
+export * from "./BiometricProfile";
+export * from "./CertificateAuthorityEngine";
+export * from "./DecentralizedIdentifierEngine";
+export * from "./DigitalIdentity";
+export * from "./IdentityFederationEngine";
+export * from "./IdentityLifecycleEngine";
+export * from "./IdentityTrustOperationsOrchestrator";
+export * from "./PrivacyConsentEngine";
+export * from "./SovereignIdentityEngine";
+export * from "./VerifiableCredential";
+export * from "./VerifiableCredentialEngine";
+export * from "./ZeroTrustIdentityEngine";

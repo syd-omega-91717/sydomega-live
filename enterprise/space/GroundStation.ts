267aee846f819ed751fda5648b7008f29e071d8c
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 08:14:39 +0300

    Create GroundStation interface for space module

---
+++ enterprise/space/GroundStation.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/space/GroundStation.ts
+// ============================================================================
+
+export interface GroundStation{
+
+    id:string;
+
+    name:string;
+
+    latitude:number;
+
+    longitude:number;
+
+    operational:boolean;
+
+}

b806fd2779b325365ef345b0dec9b2f6ef6c591f
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 11:07:41 +0300

    Create IndustrialAsset interface for asset management

---
+++ enterprise/manufacturing/IndustrialAsset.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/manufacturing/IndustrialAsset.ts
+// ============================================================================
+
+export interface IndustrialAsset{
+
+    id:string;
+
+    name:string;
+
+    assetType:string;
+
+    operational:boolean;
+
+    location:string;
+
+}

122e85e99df71dcdd98823a65c6cfa567646f5f8
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 19:24:09 +0300

    Add BiometricProfile interface definition

---
+++ enterprise/identity/BiometricProfile.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE:
+// /enterprise/identity/BiometricProfile.ts
+// ============================================================================
+
+export interface BiometricProfile{
+
+    id:string;
+
+    identityId:string;
+
+    biometricType:string;
+
+    verified:boolean;
+
+}

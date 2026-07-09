887c10da8728f250d23364e50167768341126c82
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 20:34:35 +0300

    Create BusinessRule interface for automation module

---
+++ enterprise/automation/BusinessRule.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE:
+// /enterprise/automation/BusinessRule.ts
+// ============================================================================
+
+export interface BusinessRule{
+
+    id:string;
+
+    name:string;
+
+    decisionModel:string;
+
+    enabled:boolean;
+
+}

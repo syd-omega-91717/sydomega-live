a3d91a8c2ea1232b445c425c321a6b98dd3ab2b9
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 20:24:59 +0300

    Create DataLake.ts

---
+++ enterprise/data/DataLake.ts
@@ -0,0 +1,19 @@
+// ============================================================================
+// ENTERPRISE CORE EC-048
+// FILE:
+// /enterprise/data/DataLake.ts
+// ============================================================================
+
+export interface DataLake{
+
+    id:string;
+
+    name:string;
+
+    storageProvider:string;
+
+    region:string;
+
+    status:string;
+
+}

0ee821554b0205364c070a7a45f2e8f25f144912
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 22:08:12 +0300

    Create PlatformService interface for platform details

---
+++ enterprise/kernel/PlatformService.ts
@@ -0,0 +1,19 @@
+// ============================================================================
+// ENTERPRISE CORE EC-055
+// FILE:
+// /enterprise/kernel/PlatformService.ts
+// ============================================================================
+
+export interface PlatformService{
+
+    id:string;
+
+    name:string;
+
+    version:string;
+
+    domain:string;
+
+    healthy:boolean;
+
+}

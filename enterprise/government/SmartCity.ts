574ffb58f0bda3b4217e8c444b894dc4ea3cd31d
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 10:29:26 +0300

    Create SmartCity interface for government module

---
+++ enterprise/government/SmartCity.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE:
+// /enterprise/government/SmartCity.ts
+// ============================================================================
+
+export interface SmartCity{
+
+    id:string;
+
+    name:string;
+
+    population:number;
+
+    digitalTwinId:string;
+
+}

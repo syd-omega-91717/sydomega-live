b98b7197de310d26d99930cbd18ba4b6e54c8081
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 20:43:21 +0300

    Create IoTDevice.ts

---
+++ enterprise/iot/IoTDevice.ts
@@ -0,0 +1,19 @@
+// ============================================================================
+// ENTERPRISE CORE EC-050
+// FILE:
+// /enterprise/iot/IoTDevice.ts
+// ============================================================================
+
+export interface IoTDevice{
+
+    id:string;
+
+    serialNumber:string;
+
+    deviceType:string;
+
+    firmwareVersion:string;
+
+    status:string;
+
+}

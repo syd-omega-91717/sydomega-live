76ca36635adce57a684572437b83fe23128da612
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 19:59:45 +0300

    Add index.ts to export communication modules

---
+++ enterprise/communications/index.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/communications/index.ts
+// ============================================================================
+
+export * from "./CollaborationEngine";
+export * from "./CommunicationsOperationsOrchestrator";
+export * from "./EmailServicesEngine";
+export * from "./EnterpriseMessagingEngine";
+export * from "./EnterpriseSocialEngine";
+export * from "./KnowledgeArticle";
+export * from "./KnowledgeManagementEngine";
+export * from "./Meeting";
+export * from "./Message";
+export * from "./NotificationHubEngine";
+export * from "./VideoConferenceEngine";
+export * from "./VoIPEngine";
+export * from "./WikiDocumentationEngine";

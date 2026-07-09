84b1a6eb244e1e7fec803173649bc5e0a33d4200
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 16:12:39 +0300

    Create SalesOrder interface for commerce module

---
+++ enterprise/commerce/SalesOrder.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/commerce/SalesOrder.ts
+// ============================================================================
+
+export interface SalesOrder{
+
+    id:string;
+
+    customerId:string;
+
+    total:number;
+
+    currency:string;
+
+    status:string;
+
+}

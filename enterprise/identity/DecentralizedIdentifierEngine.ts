b60c535623e4c6dc0326dc8e95283535016f79d6
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 19:25:15 +0300

    Create DecentralizedIdentifierEngine class

---
+++ enterprise/identity/DecentralizedIdentifierEngine.ts
@@ -0,0 +1,24 @@
+// ============================================================================
+// FILE:
+// /enterprise/identity/DecentralizedIdentifierEngine.ts
+// ============================================================================
+
+export class DecentralizedIdentifierEngine{
+
+    resolve(
+
+        did:string
+
+    ){
+
+        return{
+
+            did,
+
+            resolved:true
+
+        };
+
+    }
+
+}

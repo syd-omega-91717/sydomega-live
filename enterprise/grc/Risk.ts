61790689e455433ec218e80858592d5d703355a8
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 19:13:49 +0300

    Create Risk interface for risk management

---
+++ enterprise/grc/Risk.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/grc/Risk.ts
+// ============================================================================
+
+export interface Risk{
+
+    id:string;
+
+    title:string;
+
+    category:string;
+
+    likelihood:number;
+
+    impact:number;
+
+}

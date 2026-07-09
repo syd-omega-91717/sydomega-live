be293f715aea25a6db3654c21babe8778bb9bd5e
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 19:23:13 +0300

    Add DigitalIdentity interface definition

---
+++ enterprise/identity/DigitalIdentity.ts
@@ -0,0 +1,19 @@
+// ============================================================================
+// ENTERPRISE CORE EC-044
+// FILE:
+// /enterprise/identity/DigitalIdentity.ts
+// ============================================================================
+
+export interface DigitalIdentity{
+
+    id:string;
+
+    did:string;
+
+    ownerId:string;
+
+    status:string;
+
+    createdAt:string;
+
+}

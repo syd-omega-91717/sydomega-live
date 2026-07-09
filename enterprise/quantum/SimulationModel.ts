ec7e448e2a55580bfd95f2b8549eb341d7eaa764
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 08:04:27 +0300

    Create SimulationModel interface for quantum simulations

---
+++ enterprise/quantum/SimulationModel.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE:
+// /enterprise/quantum/SimulationModel.ts
+// ============================================================================
+
+export interface SimulationModel{
+
+    id:string;
+
+    name:string;
+
+    domain:string;
+
+    version:string;
+
+}

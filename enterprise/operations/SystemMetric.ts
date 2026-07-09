1ce8827b9b48dce74488c8190dbfba8f3cddc6fb
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 20:03:21 +0300

    Add SystemMetric interface definition

---
+++ enterprise/operations/SystemMetric.ts
@@ -0,0 +1,20 @@
+// ============================================================================
+// FILE:
+// /enterprise/operations/SystemMetric.ts
+// ============================================================================
+
+export interface SystemMetric{
+
+    id:string;
+
+    service:string;
+
+    metric:string;
+
+    value:number;
+
+    unit:string;
+
+    timestamp:string;
+
+}

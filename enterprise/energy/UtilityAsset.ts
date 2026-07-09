c870a187236b4e6dc572a778f6f38bc2d014b3b0
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 16:34:17 +0300

    Create UtilityAsset interface for energy module

---
+++ enterprise/energy/UtilityAsset.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/energy/UtilityAsset.ts
+// ============================================================================
+
+export interface UtilityAsset{
+
+    id:string;
+
+    assetType:string;
+
+    location:string;
+
+    condition:string;
+
+    critical:boolean;
+
+}

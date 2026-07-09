4492a0c0ce2c35c596e6cec7aa3d67426b227da5
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 07:29:58 +0300

    Create MarketplaceEngine class with publish method

---
+++ enterprise/commerce/MarketplaceEngine.ts
@@ -0,0 +1,24 @@
+// ============================================================================
+// FILE:
+// /enterprise/commerce/MarketplaceEngine.ts
+// ============================================================================
+
+export class MarketplaceEngine{
+
+    publish(
+
+        productId:string
+
+    ){
+
+        return{
+
+            productId,
+
+            published:true
+
+        };
+
+    }
+
+}

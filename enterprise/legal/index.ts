b430bd2f95349fb709f17bc6017f97e997caeada
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 07:48:07 +0300

    Create index.ts to export legal modules

---
+++ enterprise/legal/index.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/legal/index.ts
+// ============================================================================
+
+export * from "./BoardManagementEngine";
+export * from "./Clause";
+export * from "./ClauseLibraryEngine";
+export * from "./Contract";
+export * from "./ContractLifecycleEngine";
+export * from "./CorporateGovernanceEngine";
+export * from "./ESignatureEngine";
+export * from "./IntellectualPropertyEngine";
+export * from "./LegalCase";
+export * from "./LegalCaseManagementEngine";
+export * from "./LegalOperationsOrchestrator";
+export * from "./LitigationEngine";
+export * from "./RegulatoryIntelligenceEngine";

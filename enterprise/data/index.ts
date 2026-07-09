27950d86e8ebe44a0b194a05b8421c0ed33117d1
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 20:31:27 +0300

    Create index.ts to export data-related modules

---
+++ enterprise/data/index.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/data/index.ts
+// ============================================================================
+
+export * from "./AdvancedAnalyticsEngine";
+export * from "./BusinessIntelligenceEngine";
+export * from "./DataCatalogEntry";
+export * from "./DataLake";
+export * from "./DataQualityEngine";
+export * from "./DataWarehouse";
+export * from "./DataWarehouseEngine";
+export * from "./EnterpriseDataLakeEngine";
+export * from "./EnterpriseDataPlatformOrchestrator";
+export * from "./LakehouseEngine";
+export * from "./MasterDataManagementEngine";
+export * from "./MetadataCatalogEngine";
+export * from "./SemanticKnowledgeLayerEngine";

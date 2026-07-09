3051b71ee440dcdedbb7b3312f405b1f07c0e06a
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 10:51:04 +0300

    Create Asset interface for asset management

---
+++ enterprise/defense/Asset.ts
@@ -0,0 +1,16 @@
+// ============================================================================
+// FILE:
+// /enterprise/defense/Asset.ts
+// ============================================================================
+
+export interface Asset{
+
+    id:string;
+
+    name:string;
+
+    classification:string;
+
+    operational:boolean;
+
+}

4c1e3f50a9f3fead666d2d2ce0521798348baad0
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 22:08:39 +0300

    Create SystemPolicy interface in TypeScript

---
+++ enterprise/kernel/SystemPolicy.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/kernel/SystemPolicy.ts
+// ============================================================================
+
+export interface SystemPolicy{
+
+    id:string;
+
+    name:string;
+
+    scope:string;
+
+    enabled:boolean;
+
+    revision:string;
+
+}

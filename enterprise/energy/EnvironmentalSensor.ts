e70f124192e744cc6084002b92cf9c1a957f6a1f
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 16:34:45 +0300

    Add EnvironmentalSensor interface definition

---
+++ enterprise/energy/EnvironmentalSensor.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/energy/EnvironmentalSensor.ts
+// ============================================================================
+
+export interface EnvironmentalSensor{
+
+    id:string;
+
+    sensorType:string;
+
+    latitude:number;
+
+    longitude:number;
+
+    online:boolean;
+
+}

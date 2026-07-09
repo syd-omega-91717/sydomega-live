c091fe20a904f71fac82fca959fb7de0dad8f4c6
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 20:49:53 +0300

    Add index.ts for IoT module exports

---
+++ enterprise/iot/index.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/iot/index.ts
+// ============================================================================
+
+export * from "./AutonomousDeviceManagementEngine";
+export * from "./DeviceProvisioningEngine";
+export * from "./DigitalSensorNetworkEngine";
+export * from "./EdgeComputingRuntimeEngine";
+export * from "./EdgeNode";
+export * from "./IndustrialIoTEngine";
+export * from "./IoTDevice";
+export * from "./IoTDeviceRegistryEngine";
+export * from "./IoTEdgeOperationsOrchestrator";
+export * from "./PredictiveMaintenanceEngine";
+export * from "./Sensor";
+export * from "./SmartBuildingAutomationEngine";
+export * from "./SmartCityInfrastructureEngine";

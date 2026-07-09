d67705230d271c48f2fc78be6f02486c543c4cbc
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 20:25:33 +0300

    Create DataWarehouse interface in DataWarehouse.ts

---
+++ enterprise/data/DataWarehouse.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/data/DataWarehouse.ts
+// ============================================================================
+
+export interface DataWarehouse{
+
+    id:string;
+
+    name:string;
+
+    engine:string;
+
+    schemaVersion:string;
+
+    status:string;
+
+}

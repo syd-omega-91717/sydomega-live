e9c824d3976bd8dfc3bab0ebf70e17b7f884b0d0
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 20:53:24 +0300

    Create DeveloperApplication interface

---
+++ enterprise/api/DeveloperApplication.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/api/DeveloperApplication.ts
+// ============================================================================
+
+export interface DeveloperApplication{
+
+    id:string;
+
+    name:string;
+
+    ownerId:string;
+
+    apiKey:string;
+
+    active:boolean;
+
+}

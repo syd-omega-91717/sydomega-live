87138995ea1c84cf19d20d39d6f7f32a8f5ec074
S.Y.D OMEGA 91717 <s.y.dagher@gmail.com>
2026-07-09 07:28:06 +0300

    Create Vendor interface for commerce module

---
+++ enterprise/commerce/Vendor.ts
@@ -0,0 +1,18 @@
+// ============================================================================
+// FILE:
+// /enterprise/commerce/Vendor.ts
+// ============================================================================
+
+export interface Vendor{
+
+    id:string;
+
+    name:string;
+
+    country:string;
+
+    rating:number;
+
+    verified:boolean;
+
+}
